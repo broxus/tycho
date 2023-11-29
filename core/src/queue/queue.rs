6a12a5568ffbb1b21d451f50b955d82322a336a6
Maksim Greshniakov <infiltrator53@gmail.com>
2023-11-29 12:42:46 +0100

    review fixes

--- core/src/queue/queue.rs
+++ core/src/queue/queue.rs
@@ -1,31 +1,49 @@
 use std::path::PathBuf;
 
 type ShardIdent = i64;
-type LtHash = u64;
+type Lt = u64;
+
+// need to load types from types crate
+type MessageHash = String;
+type Address = String;
 
 struct QueueDiff {}
 struct Message {}
 struct QueueIterator {}
 
+struct MessageEnvelope {
+    lt: u64,
+    hash: String,
+    message: Message,
+    from_contract: Address,
+    to_contract: Address,
+}
+
 trait MessageQueue {
     // Factory methods for initialization and loading
-    fn new(directory: PathBuf, shard_id: ShardIdent) -> Self;
-    fn load_from_storage(directory: PathBuf, shard_id: ShardIdent) -> Self;
+    fn init(directory: PathBuf, shard_id: ShardIdent, load_if_exists: bool) -> Self;
 
     // Methods for queue management
     fn apply_diff(&mut self, diff: QueueDiff);
-    fn save_to_storage(&self, directory: PathBuf);
+    fn save_to_storage(&self);
     fn commit_current_state(&mut self);
 
     // Differential and state management
-    fn get_current_diff(&self) -> Option<Vec<QueueDiff>>;
+    fn get_current_diff(&self) -> Option<QueueDiff>;
     fn undo_state_to_block(&mut self, diff: Vec<QueueDiff>);
 
     // Message handling
-    fn add_message(&mut self, lt_hash: LtHash, message: Message);
-    fn upto_message(&mut self, lt_hash: LtHash);
+    fn add_message(&mut self, message: MessageEnvelope);
+    fn add_processed_upto(&mut self, lt: Lt, hash: MessageHash);
 
-    // Queue navigation and sharding
-    fn create_iterator(&self, lt_hash: LtHash) -> QueueIterator;
-    fn next_element(&self, iterator: &QueueIterator) -> Option<Message>;
+    // Queue navigation
+    fn create_iterator(&self) -> QueueIterator;
+}
+
+impl Iterator for QueueIterator {
+    type Item = Message;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
 }
