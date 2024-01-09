7cab6d2c7679f93e38093b8e95c5323d1bb48c82
Vitaly Terekhov <va.tehb@gmail.com>
2024-01-09 21:15:17 +0000

    new msg queue module structure

--- core/src/msg_queue/mod.rs
+++ core/src/msg_queue/mod.rs
@@ -1,1 +1,18 @@
+pub mod config;
+pub mod types;
+
+mod diff_mgmt;
+mod iterator;
+mod loader;
 mod queue;
+
+pub mod cache_persistent;
+mod cache_persistent_fs;
+
+pub mod state_persistent;
+mod state_persistent_fs;
+
+pub mod storage;
+mod storage_rocksdb;
+
+pub use {diff_mgmt::*, iterator::*, queue::*};
