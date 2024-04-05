b434df98dfa4a9c22b96df1cdccd2f0122905599
Stanislav Eliseev <s.eliseev@dexpa.io>
2024-04-05 12:09:31 +0200

    feat: move overlay client from supersonar with updates - wip

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod block_strider;
+pub mod internal_queue;
 mod overlay;
-mod proto;
--- core/src/overlay/public_overlay_client.rs
+++ core/src/overlay/public_overlay_client.rs
@@ -2,23 +2,19 @@ use std::future::Future;
 use std::sync::Arc;
 use std::time::Duration;
 
-use anyhow::{Error, Result};
-use tl_proto::{Boxed, TlRead, TlWrite};
-
-use tycho_network::{NetworkExt, PeerId, PublicOverlay, Request};
-use tycho_network::Network;
-
 use crate::overlay::neighbours::{NeighbourCollection, Neighbours};
-use crate::proto::overlay::{Ping, Pong};
-
+use tycho_network::Network;
+use tycho_network::__internal::tl_proto::{TlRead, TlWrite};
+use tycho_network::{NetworkExt, PeerId, PublicOverlay};
+use tycho_util::FastDashMap;
 
 trait OverlayClient {
-    async fn send<R>(&self, data: R) -> Result<()> where
-        R: tl_proto::TlWrite<Repr = tl_proto::Boxed>;
+    async fn send<R: TlWrite>(&self, data: R) -> Box<dyn Future<Output = ()>>;
 
-    async fn query<R, A>(&self, data: R) -> Result<A> where
-        R: tl_proto::TlWrite<Repr = tl_proto::Boxed>,
-        for<'a> A: TlRead<'a>,;
+    async fn query<R, A>(&self, data: R) -> Box<dyn Future<Output = Option<A>>>
+    where
+        R: TlWrite,
+        for<'a> A: TlRead<'a>;
 }
 
 #[derive(Clone)]
@@ -33,89 +29,18 @@ impl PublicOverlayClient {
         }))
     }
 
-    pub async fn ping_random_neighbour(&self) -> Result<()> {
-        let Some(neighbour) = self.0.neighbours.0.choose().await else {
-            tracing::error!("No neighbours found to ping");
-            return Err(Error::msg("Failed to ping"));
-        };
-
-        tracing::info!(
-            peer_id = %neighbour.peer_id(),
-            stats = ?neighbour.get_stats(),
-            "Selected neighbour to ping",
-        );
-        let start_time =  tycho_util::time::now_millis();
-        let ping = Ping {
-            value: start_time
-        };
-
-
-        let pong_res = self.0.overlay.query(&self.0.network, neighbour.peer_id(), Request::from_tl(ping)).await;
-
-        let end_time = tycho_util::time::now_millis();
-
-       let (request_time, success) =  match pong_res {
-            Ok(response) => {
-                let pong: Pong = response.parse_tl::<Pong>()?;
-                tracing::info!(peer_id = %neighbour.peer_id(), "Pong received", );
-                (pong.value - start_time, true)
-            }
-            Err(e) => {
-                tracing::error!(peer_id = %neighbour.peer_id(), "Failed to received pong. Error: {e:?}");
-                (u64::MAX, false)
-            }
-        };
-
-        neighbour.track_request(request_time, end_time - start_time, success);
-        self.0.neighbours.0.update_selection_index().await;
-
-        Ok(())
-
-    }
-
+    pub fn ping_neighbour(&self, peer: &PeerId) {}
 }
 
 impl OverlayClient for PublicOverlayClient {
-    async fn send<R>(&self, data: R) -> Result<()>
-        where
-            R: tl_proto::TlWrite<Repr = tl_proto::Boxed>,
-    {
-        let Some(neighbour) = self.0.neighbours.0.choose().await else {
-            tracing::error!("No neighbours found to send request");
-            return Err(Error::msg("Failed to ping")); //TODO: proper error
-        };
-
-        //let boxed = tl_proto::serialize(data);
-        self.0.overlay.send(&self.0.network, neighbour.peer_id(), Request::from_tl(data)).await?;
-        Ok(())
+    async fn send<R: TlWrite>(&self, data: R) -> Box<dyn Future<Output = ()>> {
+        let neighbour = self.0.neighbours.0.choose().await;
+        self.0.overlay.send(&self.0.network)
     }
 
-    async fn query<R, A>(&self, data: R) -> Result<A> where
-        R: tl_proto::TlWrite<Repr = tl_proto::Boxed>,
-        for<'a> A: TlRead<'a>,
-    {
-        let Some(neighbour) = self.0.neighbours.0.choose().await else {
-            tracing::error!("No neighbours found to send request");
-            return Err(Error::msg("Failed to ping")); //TODO: proper error
-        };
-
-        let response_opt = self.0.overlay.query(&self.0.network, neighbour.peer_id(), Request::from_tl(data)).await;
-        match response_opt {
-            Ok(response) => {
-                Ok(response.parse_tl::<A>())
-            }
-            Err(e ) => {
-                tracing::error!(peer_id = %neighbour.peer_id(), "Failed to get response from peer. Err: {e:?}");
-                Err(e)
-            }
-        }
+    async fn query<R: TlWrite, A: TlRead>(&self, data: R) -> Box<dyn Future<Output = Option<A>>> {
+        todo!()
     }
-}
-
-pub struct NeighbourPingResult {
-    peer: PeerId,
-    request_time: u32,
-    rt_time: u32
 }
 
 struct OverlayClientState {
