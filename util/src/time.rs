ae138cb89e75aacba6cc58ea878af46258c63439
Stanislav Eliseev <s.eliseev@dexpa.io>
2024-04-08 09:34:57 +0200

    fix(overlay-client): update proto models for overlay client + misc fixes

--- util/src/time.rs
+++ util/src/time.rs
@@ -9,6 +9,13 @@ pub fn now_sec() -> u32 {
         .as_secs() as u32
 }
 
+pub fn now_millis() -> u64 {
+    std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64
+}
+
 pub fn shifted_interval(period: Duration, max_shift: Duration) -> tokio::time::Interval {
     let shift = rand::thread_rng().gen_range(Duration::ZERO..max_shift);
     tokio::time::interval_at(tokio::time::Instant::now() + shift, period + shift)
