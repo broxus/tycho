9b9de35eadeea35b239c95387761acd14f1908ef
Stanislav Eliseev <s.eliseev@dexpa.io>
2024-04-11 14:26:55 +0200

    feat(overlay-client): add peer substitution

--- core/src/overlay_client/neighbours.rs
+++ core/src/overlay_client/neighbours.rs
@@ -3,6 +3,7 @@ use rand::seq::SliceRandom;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
+use itertools::Itertools;
 use tokio::sync::Mutex;
 use tycho_network::{OverlayId, PeerId, PublicOverlay};
 use crate::overlay_client::settings::NeighboursOptions;
@@ -27,12 +28,13 @@ impl Neighbours {
             default_roundtrip_ms: options.default_roundtrip_ms,
         };
 
-        let entries = overlay.read_entries()
-            .choose_multiple(&mut rand::thread_rng(), options.max_neighbours)
-            .map(|entry_data| Neighbour::new(entry_data.entry.peer_id, neighbour_options))
-            .collect();
-
-        let entries = Mutex::new(entries);
+        let entries = {
+            let entries = overlay.read_entries()
+                .choose_multiple(&mut rand::thread_rng(), options.max_neighbours)
+                .map(|entry_data| Neighbour::new(entry_data.entry.peer_id, neighbour_options))
+                .collect();
+            Mutex::new(entries)
+        };
 
         let selection_index = Mutex::new(SelectionIndex::new(options.max_neighbours));
 
@@ -42,6 +44,7 @@ impl Neighbours {
             selection_index,
             overlay,
         };
+
         tracing::info!("Initial update selection call");
         result.update_selection_index().await;
         tracing::info!("Initial update selection finished");
@@ -60,57 +63,82 @@ impl Neighbours {
             .get(&mut rand::thread_rng())
     }
 
+
+
     pub async fn update_selection_index(&self) {
-        let guard = self.entries.lock().await;
+        let mut guard = self.entries.lock().await;
+        guard.retain(|x| x.is_reliable());
         let mut lock = self.selection_index.lock().await;
         lock.update(guard.as_slice());
     }
 
     pub async fn get_sorted_neighbours(&self) ->  Vec<(Neighbour, u32)> {
         let mut index = self.selection_index.lock().await;
-        index.indices_with_weights.sort_by(|(ln, lw), (rn, rw) | lw.cmp(rw));
+        index.indices_with_weights.sort_by(|(ln, lw), (rn, rw) | rw.cmp(lw));
         return Vec::from(index.indices_with_weights.as_slice())
     }
 
-    pub async fn get_bad_neighbours_count(&self) -> usize {
-        let guard = self.entries.lock().await;
-        guard
-            .iter()
-            .filter(|x| !x.is_reliable())
-            .cloned()
-            .collect::<Vec<_>>()
-            .len()
+    pub async fn get_active_neighbours_count(&self) -> usize {
+        self.entries.lock().await.len()
     }
 
-    pub async fn update(&self, entries: &[Neighbour]) {
-        const MINIMAL_NEIGHBOUR_COUNT: usize = 16;
+    // pub async fn get_bad_neighbours_count(&self) -> usize {
+    //     let guard = self.entries.lock().await;
+    //     guard
+    //         .iter()
+    //         .filter(|x| !x.is_reliable())
+    //         .cloned()
+    //         .collect::<Vec<_>>()
+    //         .len()
+    // }
+
+    pub async fn update(&self, new: Vec<Neighbour>) {
         let mut guard = self.entries.lock().await;
+        if guard.len() >= self.options.max_neighbours {
+            // or we can alternatively remove the worst node
+            drop(guard);
+            return
+        }
 
-        guard.sort_by(|a, b| a.get_stats().score.cmp(&b.get_stats().score));
-
-        let mut all_reliable = true;
-
-        for entry in entries {
-            if let Some(index) = guard.iter().position(|x| x.peer_id() == entry.peer_id()) {
-                let nbg = guard.get(index).unwrap();
-
-                if !nbg.is_reliable() && guard.len() > MINIMAL_NEIGHBOUR_COUNT {
-                    guard.remove(index);
-                    all_reliable = false;
-                }
+        for n in new {
+            if let Some(_) = guard.iter().find(|x| x.peer_id() == n.peer_id()) {
+                continue;
+            }
+            if guard.len() < self.options.max_neighbours {
+                guard.push(n)
             } else {
-                guard.push(entry.clone());
+                return;
             }
         }
 
-        //if everything is reliable then remove the worst node
-        if all_reliable && guard.len() > MINIMAL_NEIGHBOUR_COUNT {
-            guard.pop();
-        }
-
+        // const MINIMAL_NEIGHBOUR_COUNT: usize = 16;
+        // let mut guard = self.entries.lock().await;s
+        //
+        // guard.sort_by(|a, b| a.get_stats().score.cmp(&b.get_stats().score));
+        //
+        // let mut all_reliable = true;
+        //
+        // for entry in entries {
+        //     if let Some(index) = guard.iter().position(|x| x.peer_id() == entry.peer_id()) {
+        //         let nbg = guard.get(index).unwrap();
+        //
+        //         if !nbg.is_reliable() && guard.len() > MINIMAL_NEIGHBOUR_COUNT {
+        //             guard.remove(index);
+        //             all_reliable = false;
+        //         }
+        //     } else {
+        //         guard.push(entry.clone());
+        //     }
+        // }
+        //
+        // //if everything is reliable then remove the worst node
+        // if all_reliable && guard.len() > MINIMAL_NEIGHBOUR_COUNT {
+        //     guard.pop();
+        // }
+        //
         drop(guard);
-
         self.update_selection_index().await;
+
     }
 }
 
