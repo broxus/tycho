05f08399323d66bab8e44f4e93f1d66e51de349d
Alexey Pashinov <pashinov93@gmail.com>
2024-04-12 13:18:12 +0200

    feat(overlay-server): add getNextKeyBlockIds query

--- core/src/overlay_server/mod.rs
+++ core/src/overlay_server/mod.rs
@@ -2,15 +2,74 @@ use std::sync::{Arc, Mutex};
 
 use bytes::{Buf, Bytes};
 use tokio::sync::broadcast;
-use tycho_network::proto::dht::{rpc, Value};
+use tycho_network::proto::dht::{rpc, NodeResponse, Value, ValueResponseRaw};
 use tycho_network::{Response, Service, ServiceRequest};
-use tycho_storage::Storage;
+use tycho_storage::{KeyBlocksDirection, Storage};
+use tycho_util::futures::BoxFutureOrNoop;
 
-pub struct OverlayServer(OverlayServerInner);
+use crate::proto;
+
+pub struct OverlayServer(Arc<OverlayServerInner>);
 
 impl OverlayServer {
     pub fn new(storage: Arc<Storage>) -> Arc<Self> {
-        Arc::new(Self(OverlayServerInner { storage }))
+        Arc::new(Self(Arc::new(OverlayServerInner { storage })))
+    }
+}
+
+impl Service<ServiceRequest> for OverlayServer {
+    type QueryResponse = Response;
+    type OnQueryFuture = BoxFutureOrNoop<Option<Self::QueryResponse>>;
+    type OnMessageFuture = futures_util::future::Ready<()>;
+    type OnDatagramFuture = futures_util::future::Ready<()>;
+
+    #[tracing::instrument(
+        level = "debug",
+        name = "on_overlay_server_query",
+        skip_all,
+        fields(peer_id = %req.metadata.peer_id, addr = %req.metadata.remote_address)
+    )]
+    fn on_query(&self, req: ServiceRequest) -> Self::OnQueryFuture {
+        let (constructor, body) = match self.0.try_handle_prefix(&req) {
+            Ok(rest) => rest,
+            Err(e) => {
+                tracing::debug!("failed to deserialize query: {e}");
+                return BoxFutureOrNoop::Noop;
+            }
+        };
+
+        tycho_network::match_tl_request!(body, tag = constructor, {
+            proto::overlay::GetNextKeyBlockIds as req => {
+                BoxFutureOrNoop::future({
+                    tracing::debug!(blockId = %req.block, max_size = req.max_size, "keyBlocksRequest");
+
+                    let inner = self.0.clone();
+
+                    async move {
+                        let res = inner.handle_get_next_key_block_ids(&req);
+                        Some(Response::from_tl(res))
+
+                        /*tokio::task::spawn_blocking(move || {
+                            let res = inner.handle_get_next_key_block_ids(&req);
+                            Some(Response::from_tl(res))
+                        }).await.unwrap_or_else(|_| None)*/
+                    }
+                })
+            },
+        }, e => {
+            tracing::debug!("failed to deserialize query: {e}");
+            BoxFutureOrNoop::Noop
+        })
+    }
+
+    #[inline]
+    fn on_message(&self, _req: ServiceRequest) -> Self::OnMessageFuture {
+        futures_util::future::ready(())
+    }
+
+    #[inline]
+    fn on_datagram(&self, _req: ServiceRequest) -> Self::OnDatagramFuture {
+        futures_util::future::ready(())
     }
 }
 
@@ -19,6 +78,10 @@ struct OverlayServerInner {
 }
 
 impl OverlayServerInner {
+    fn storage(&self) -> &Storage {
+        self.storage.as_ref()
+    }
+
     fn try_handle_prefix<'a>(&self, req: &'a ServiceRequest) -> anyhow::Result<(u32, &'a [u8])> {
         let mut body = req.as_ref();
         anyhow::ensure!(body.len() >= 4, tl_proto::TlError::UnexpectedEof);
@@ -27,68 +90,70 @@ impl OverlayServerInner {
 
         Ok((constructor, body))
     }
+
+    fn handle_get_next_key_block_ids(
+        &self,
+        req: &proto::overlay::GetNextKeyBlockIds,
+    ) -> proto::overlay::Response<proto::overlay::KeyBlockIdsResponse> {
+        const NEXT_KEY_BLOCKS_LIMIT: usize = 8;
+
+        let block_handle_storage = self.storage().block_handle_storage();
+
+        let limit = std::cmp::min(req.max_size as usize, NEXT_KEY_BLOCKS_LIMIT);
+
+        let get_next_key_block_ids = || {
+            let start_block_id = &req.block;
+            if !start_block_id.shard.is_masterchain() {
+                return Err(OverlayServerError::BlockNotFromMasterChain.into());
+            }
+
+            let mut iterator = block_handle_storage
+                .key_blocks_iterator(KeyBlocksDirection::ForwardFrom(start_block_id.seqno))
+                .take(limit)
+                .peekable();
+
+            if let Some(Ok(id)) = iterator.peek() {
+                if id.root_hash != start_block_id.root_hash {
+                    return Err(OverlayServerError::InvalidRootHash.into());
+                }
+                if id.file_hash != start_block_id.file_hash {
+                    return Err(OverlayServerError::InvalidFileHash.into());
+                }
+            }
+
+            let mut ids = Vec::with_capacity(limit);
+            while let Some(id) = iterator.next().transpose()? {
+                ids.push(id);
+                if ids.len() >= limit {
+                    break;
+                }
+            }
+
+            Ok::<_, anyhow::Error>(ids)
+        };
+
+        match get_next_key_block_ids() {
+            Ok(ids) => {
+                let incomplete = ids.len() < limit;
+                proto::overlay::Response::Ok(proto::overlay::KeyBlockIdsResponse {
+                    blocks: ids,
+                    incomplete,
+                })
+            }
+            Err(e) => {
+                tracing::warn!("get_next_key_block_ids failed: {e:?}");
+                proto::overlay::Response::Err
+            }
+        }
+    }
 }
 
-impl Service<ServiceRequest> for OverlayServer {
-    type QueryResponse = Response;
-    type OnQueryFuture = futures_util::future::Ready<Option<Self::QueryResponse>>;
-    type OnMessageFuture = futures_util::future::Ready<()>;
-    type OnDatagramFuture = futures_util::future::Ready<()>;
-
-    #[tracing::instrument(
-    level = "debug",
-    name = "on_overlay_server_query",
-    skip_all,
-    fields(peer_id = %req.metadata.peer_id, addr = %req.metadata.remote_address)
-    )]
-    fn on_query(&self, req: ServiceRequest) -> Self::OnQueryFuture {
-        let (constructor, body) = match self.0.try_handle_prefix(&req) {
-            Ok(rest) => rest,
-            Err(e) => {
-                tracing::debug!("failed to deserialize query: {e}");
-                return futures_util::future::ready(None);
-            }
-        };
-
-        let response: Option<Vec<u8>> = tycho_network::match_tl_request!(body, tag = constructor, {
-            // TODO
-        }, e => {
-            tracing::debug!("failed to deserialize query: {e}");
-            None
-        });
-
-        futures_util::future::ready(response.map(|body| Response {
-            version: Default::default(),
-            body: Bytes::from(body),
-        }))
-    }
-
-    #[tracing::instrument(
-    level = "debug",
-    name = "on_overlay_server_message",
-    skip_all,
-    fields(peer_id = %req.metadata.peer_id, addr = %req.metadata.remote_address)
-    )]
-    fn on_message(&self, req: ServiceRequest) -> Self::OnMessageFuture {
-        let (constructor, body) = match self.0.try_handle_prefix(&req) {
-            Ok(rest) => rest,
-            Err(e) => {
-                tracing::debug!("failed to deserialize message: {e}");
-                return futures_util::future::ready(());
-            }
-        };
-
-        tycho_network::match_tl_request!(body, tag = constructor, {
-            // TODO
-        }, e => {
-            tracing::debug!("failed to deserialize message: {e}");
-        });
-
-        futures_util::future::ready(())
-    }
-
-    #[inline]
-    fn on_datagram(&self, _req: ServiceRequest) -> Self::OnDatagramFuture {
-        futures_util::future::ready(())
-    }
+#[derive(Debug, thiserror::Error)]
+enum OverlayServerError {
+    #[error("Block is not from masterchain")]
+    BlockNotFromMasterChain,
+    #[error("Invalid root hash")]
+    InvalidRootHash,
+    #[error("Invalid file hash")]
+    InvalidFileHash,
 }
--- core/src/proto/overlay.rs
+++ core/src/proto/overlay.rs
@@ -1,4 +1,5 @@
 use tl_proto::{TlRead, TlWrite};
+use tycho_network::proto::dht::rpc;
 
 #[derive(Debug, Clone, PartialEq, Eq, TlRead, TlWrite)]
 #[tl(boxed, id = "overlay.ping", scheme = "proto.tl")]
@@ -7,3 +8,106 @@ pub struct Ping;
 #[derive(Debug, Clone, PartialEq, Eq, TlRead, TlWrite)]
 #[tl(boxed, id = "overlay.pong", scheme = "proto.tl")]
 pub struct Pong;
+
+/// A universal response for the all queries.
+#[derive(Debug, Clone, PartialEq, Eq, TlRead, TlWrite)]
+#[tl(boxed, scheme = "proto.tl")]
+pub enum Response<T>
+where
+    T: tl_proto::TlWrite<Repr = tl_proto::Boxed>,
+{
+    #[tl(id = "publicOverlay.response.ok")]
+    Ok(T),
+    #[tl(id = "publicOverlay.response.error")]
+    Err,
+}
+
+#[derive(Clone, TlRead, TlWrite)]
+#[tl(boxed, id = "publicOverlay.getNextKeyBlockIds", scheme = "proto.tl")]
+pub struct GetNextKeyBlockIds {
+    #[tl(with = "tl_block_id")]
+    pub block: everscale_types::models::BlockId,
+    pub max_size: u32,
+}
+
+#[derive(Clone, TlRead, TlWrite)]
+#[tl(boxed, id = "publicOverlay.keyBlockIdsResponse", scheme = "proto.tl")]
+pub struct KeyBlockIdsResponse {
+    #[tl(with = "tl_block_id_vec")]
+    pub blocks: Vec<everscale_types::models::BlockId>,
+    pub incomplete: bool,
+}
+
+mod tl_block_id {
+    use everscale_types::models::{BlockId, ShardIdent};
+    use everscale_types::prelude::HashBytes;
+    use tl_proto::{TlPacket, TlRead, TlResult, TlWrite};
+
+    pub const SIZE_HINT: usize = 80;
+
+    pub const fn size_hint(_: &BlockId) -> usize {
+        SIZE_HINT
+    }
+
+    pub fn write<P: TlPacket>(block_id: &BlockId, packet: &mut P) {
+        block_id.shard.workchain().write_to(packet);
+        block_id.shard.prefix().write_to(packet);
+        block_id.seqno.write_to(packet);
+        block_id.root_hash.0.write_to(packet);
+        block_id.file_hash.0.write_to(packet);
+    }
+
+    pub fn read(packet: &[u8], offset: &mut usize) -> TlResult<BlockId> {
+        let workchain = i32::read_from(packet, offset)?;
+        let prefix = u64::read_from(packet, offset)?;
+        let seqno = u32::read_from(packet, offset)?;
+
+        let shard = ShardIdent::new(workchain, prefix);
+
+        let shard = match shard {
+            None => return Err(tl_proto::TlError::InvalidData),
+            Some(shard) => shard,
+        };
+
+        let root_hash = HashBytes(<[u8; 32]>::read_from(packet, offset)?);
+        let file_hash = HashBytes(<[u8; 32]>::read_from(packet, offset)?);
+
+        Ok(BlockId {
+            shard,
+            seqno,
+            root_hash,
+            file_hash,
+        })
+    }
+}
+
+mod tl_block_id_vec {
+    use everscale_types::models::BlockId;
+    use tl_proto::{TlError, TlPacket, TlRead, TlResult};
+
+    use crate::proto::overlay::tl_block_id;
+
+    pub fn size_hint(ids: &[BlockId]) -> usize {
+        4 + ids.len() * tl_block_id::SIZE_HINT
+    }
+
+    pub fn write<P: TlPacket>(blocks: &[BlockId], packet: &mut P) {
+        packet.write_u32(blocks.len() as u32);
+        for block in blocks {
+            tl_block_id::write(block, packet);
+        }
+    }
+
+    pub fn read(packet: &[u8], offset: &mut usize) -> TlResult<Vec<BlockId>> {
+        let len = u32::read_from(packet, offset)?;
+        if *offset + len as usize * tl_block_id::SIZE_HINT > packet.len() {
+            return Err(TlError::UnexpectedEof);
+        }
+
+        let mut ids = Vec::with_capacity(len as usize);
+        for _ in 0..len {
+            ids.push(tl_block_id::read(packet, offset)?);
+        }
+        Ok(ids)
+    }
+}
