b93c09524c8b11d0b128dbc70de9883244829f45
Vitaly Terekhov <va.tehb@gmail.com>
2024-06-19 17:31:32 +0000

    fix(collator): fixed calculated int msgs queue length

--- collator/src/collator/do_collate.rs
+++ collator/src/collator/do_collate.rs
@@ -530,7 +530,7 @@ impl CollatorStdImpl {
             .increment(collation_data.int_enqueue_count as _);
         metrics::counter!("tycho_do_collate_int_dequeue_count")
             .increment(collation_data.int_dequeue_count as _);
-        metrics::gauge!("tycho_do_collate_int_msgs_queue_calc", labels).increment(
+        metrics::gauge!("tycho_do_collate_int_msgs_queue_calc").increment(
             (collation_data.int_enqueue_count as i64 - collation_data.int_dequeue_count as i64)
                 as f64,
         );
