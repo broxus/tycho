56969a811b446ce6f0618ade6762672c3c84cb4f
Ivan Kalinin <i.kalinin@dexpa.io>
2024-07-25 16:12:21 +0200

    chore(cli): replace `public-ip` with `getip`

--- cli/src/node/mod.rs
+++ cli/src/node/mod.rs
@@ -271,9 +271,9 @@ fn init_metrics(config: &MetricsConfig) -> Result<()> {
 async fn resolve_public_ip(ip: Option<IpAddr>) -> Result<IpAddr> {
     match ip {
         Some(address) => Ok(address),
-        None => match public_ip::addr_v4().await {
-            Some(address) => Ok(IpAddr::V4(address)),
-            None => anyhow::bail!("failed to resolve public IP address"),
+        None => match getip::addr_v4().await {
+            Ok(address) => Ok(IpAddr::V4(address)),
+            Err(e) => anyhow::bail!("failed to resolve public IP address: {e:?}"),
         },
     }
 }
