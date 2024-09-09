ae2e23b24554fee364b3bede71d9b1e9d170c0ba
serejkaaa512 <5125402@mail.ru>
2024-09-09 11:28:18 +0300

    feat(collator): add queue diff to tests boc

--- collator/src/test_utils.rs
+++ collator/src/test_utils.rs
@@ -1,5 +1,9 @@
-use everscale_types::boc::Boc;
-use everscale_types::models::{BlockId, ShardStateUnsplit};
+use everscale_types::boc::{Boc, BocRepr};
+use everscale_types::cell::CellBuilder;
+use everscale_types::models::{Block, BlockId, ShardStateUnsplit};
+use tycho_block_util::archive::ArchiveData;
+use tycho_block_util::block::BlockStuff;
+use tycho_block_util::queue::{QueueDiffStuff, QueueDiffStuffAug};
 use tycho_block_util::state::{MinRefMcStateTracker, ShardStateStuff};
 use tycho_storage::{NewBlockMeta, Storage};
 
@@ -27,10 +31,14 @@ pub async fn prepare_test_storage() -> anyhow::Result<Storage> {
     let tracker = MinRefMcStateTracker::default();
 
     // master state
-    let master_bytes = include_bytes!("../../test/test_state_3_master.boc");
-    let master_file_hash = Boc::file_hash_blake(master_bytes);
-    let master_root = Boc::decode(master_bytes)?;
-    let master_root_hash = *master_root.repr_hash();
+    let zerostate = Boc::decode(include_bytes!("../../test/zerostate.boc"))?;
+
+    let master_block_data = include_bytes!("../../test/first-block.boc");
+    let master_block: Block = BocRepr::decode(master_block_data)?;
+
+    let master_id: BlockId = "-1:8000000000000000:1:98853ae1fa6eb846610381f1a96320325c8557831152279755208435dc494eec:597a90796b17f9d1d55fd2a8ac046bbca7e2104e961778859ad935454dbf62c0".parse()?;
+
+    let master_root = master_block.load_state_update()?.apply(&zerostate)?;
     let master_state = master_root.parse::<Box<ShardStateUnsplit>>()?;
 
     let mc_state_extra = master_state.load_custom()?;
@@ -38,27 +46,39 @@ pub async fn prepare_test_storage() -> anyhow::Result<Storage> {
     let shard_info_opt = mc_state_extra.shards.iter().next();
     let shard_info = shard_info_opt.unwrap().unwrap();
 
-    let master_id = BlockId {
-        shard: master_state.shard_ident,
-        seqno: master_state.seqno,
-        root_hash: master_root_hash,
-        file_hash: master_file_hash,
-    };
     let master_state_stuff =
         ShardStateStuff::from_state_and_root(&master_id, master_state, master_root, &tracker)?;
 
-    let (handle, _) =
-        storage
-            .block_handle_storage()
-            .create_or_load_handle(&master_id, NewBlockMeta {
-                is_key_block: mc_state_extra.after_key_block,
-                gen_utime: master_state_stuff.state().gen_utime,
-                mc_ref_seqno: Some(master_id.seqno),
-            });
+    let meta_data = NewBlockMeta {
+        is_key_block: mc_state_extra.after_key_block,
+        gen_utime: master_state_stuff.state().gen_utime,
+        mc_ref_seqno: Some(master_id.seqno),
+    };
+    let (handle, _) = storage
+        .block_handle_storage()
+        .create_or_load_handle(&master_id, meta_data);
 
     storage
         .shard_state_storage()
         .store_state(&handle, &master_state_stuff)
+        .await?;
+
+    let root = CellBuilder::build_from(&master_block)?;
+    let data = everscale_types::boc::Boc::encode_rayon(&root);
+    let block_stuff = BlockStuff::from_block_and_root(&master_id, master_block, root);
+    let handle = storage
+        .block_storage()
+        .store_block_data(&block_stuff, &ArchiveData::New(data.into()), meta_data)
+        .await?
+        .handle;
+
+    let queue_data = include_bytes!("../../test/first-block-queue-diff.boc");
+    let stuff = QueueDiffStuff::deserialize(&master_id, queue_data)?;
+    let stuff_aug = QueueDiffStuffAug::new(stuff, queue_data.to_vec());
+
+    storage
+        .block_storage()
+        .store_queue_diff(&stuff_aug, handle.into())
         .await?;
 
     // shard state
