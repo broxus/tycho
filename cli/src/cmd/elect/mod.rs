e02b2298f88f01c20a979291d359cd070f7d91c2
Ivan Kalinin <i.kalinin@dexpa.io>
2024-10-29 20:11:56 +0100

    feat(cli): add semi-reliable broadcast mode for `elect` messages

--- cli/src/cmd/elect/mod.rs
+++ cli/src/cmd/elect/mod.rs
@@ -1,6 +1,7 @@
 use std::future::Future;
 use std::path::PathBuf;
 use std::sync::{Arc, OnceLock};
+use std::time::Duration;
 
 use anyhow::{Context, Result};
 use clap::{Args, Parser, Subcommand};
@@ -9,8 +10,8 @@ use everscale_types::abi::{
     extend_signature_with_id, AbiType, AbiValue, AbiVersion, FromAbi, IntoAbi, WithAbiType,
 };
 use everscale_types::models::{
-    Account, AccountState, BlockchainConfig, ElectionTimings, ExtInMsgInfo, GlobalCapability,
-    MsgInfo, OwnedMessage, StdAddr, ValidatorStakeParams,
+    Account, AccountState, BlockchainConfig, ExtInMsgInfo, GlobalCapability, MsgInfo, OwnedMessage,
+    StateInit, StdAddr, ValidatorStakeParams,
 };
 use everscale_types::prelude::*;
 use reqwest::Url;
@@ -76,6 +77,10 @@ struct CmdOnce {
     /// Max stake factor. Uses config by default.
     #[clap(long)]
     max_factor: Option<u32>,
+
+    /// Skip waiting for the message delivery.
+    #[clap(long)]
+    no_wait: bool,
 }
 
 impl CmdOnce {
@@ -104,57 +109,89 @@ impl CmdOnce {
             };
 
             // Get current elections
-            let elector_data = client.get_elector_data().await?;
+            let (_, elector_data) = client.get_elector_data().await?;
             let Some(Ref(elections)) = elector_data.current_election else {
                 return print_json(ParticipateStatus::NoElections);
             };
 
-            // Build payload signature
+            // Check for an existing stake
             let adnl_addr = HashBytes(adnl_addr.to_bytes());
             let validator_key = adnl_addr;
-            let signature = {
-                let data_to_sign = build_elections_data_to_sign(
-                    elections.elect_at,
-                    max_factor,
-                    &wallet.address.address,
-                    &adnl_addr,
+
+            let mut existing_stake = 0;
+            let mut update_member = true;
+            if let Some(member) = elections.members.get(&validator_key) {
+                anyhow::ensure!(
+                    member.src_addr == wallet.address.address,
+                    "can make stakes for a public key from one address only"
                 );
-                let data_to_sign = extend_signature_with_id(&data_to_sign, config.signature_id);
-                node_keys.sign_raw(&data_to_sign).to_vec()
-            };
 
-            // Build elections payload
-            let payload = elector::methods::participate_in_elections()
-                .encode_internal_input(&[ParticiateInElectionsInput {
-                    query_id: now_millis(),
-                    validator_key,
-                    stake_at: elections.elect_at,
-                    max_factor,
-                    adnl_addr,
-                    signature,
+                existing_stake = member.msg_value.into_inner();
+                update_member = adnl_addr != member.adnl_addr || max_factor != member.max_factor;
+            }
+
+            let stake_diff = self.stake.saturating_sub(existing_stake);
+            anyhow::ensure!(
+                (stake_diff << 12) >= elections.total_stake.into_inner(),
+                "stake diff is too small"
+            );
+
+            let message = if stake_diff > 0 || update_member {
+                // Build payload signature
+                let signature = {
+                    let data_to_sign = build_elections_data_to_sign(
+                        elections.elect_at,
+                        max_factor,
+                        &wallet.address.address,
+                        &adnl_addr,
+                    );
+                    let data_to_sign = extend_signature_with_id(&data_to_sign, config.signature_id);
+                    node_keys.sign_raw(&data_to_sign).to_vec()
+                };
+
+                // Build elections payload
+                let payload = elector::methods::participate_in_elections()
+                    .encode_internal_input(&[ParticiateInElectionsInput {
+                        query_id: now_millis(),
+                        validator_key,
+                        stake_at: elections.elect_at,
+                        max_factor,
+                        adnl_addr,
+                        signature,
+                    }
+                    .into_abi()
+                    .named("input")])?
+                    .build()?;
+
+                // Send stake
+                let message = wallet
+                    .transfer(
+                        &ELECTOR_ADDR,
+                        Amount::Exact(stake_diff + ONE_CC),
+                        true,
+                        payload,
+                        config.signature_id,
+                    )
+                    .await?;
+
+                if !self.no_wait {
+                    wallet.wait_delivered(&message).await?;
                 }
-                .into_abi()
-                .named("input")])?
-                .build()?;
 
-            // Send stake
-            let sent = wallet
-                .transfer(
-                    &ELECTOR_ADDR,
-                    Amount::Exact(self.stake + ONE_CC),
-                    true,
-                    payload,
-                    config.signature_id,
-                )
-                .await?;
+                Some(message)
+            } else {
+                None
+            };
 
             // Done
             print_json(ParticipateStatus::Participating {
-                message_hash: sent.hash,
-                message_expire_at: sent.expire_at,
+                message,
                 elections_id: elections.elect_at,
                 elections_end: elections.elect_close,
-                stake: self.stake,
+                public: validator_key,
+                adnl_addr,
+                stake: existing_stake + stake_diff,
+                max_factor,
             })
         })
     }
@@ -165,12 +202,14 @@ impl CmdOnce {
 enum ParticipateStatus {
     NoElections,
     Participating {
-        message_hash: HashBytes,
-        message_expire_at: u32,
+        message: Option<SentMessage>,
         elections_id: u32,
         elections_end: u32,
+        public: HashBytes,
+        adnl_addr: HashBytes,
         #[serde(with = "serde_helpers::string")]
         stake: u128,
+        max_factor: u32,
     },
 }
 
@@ -183,6 +222,10 @@ struct CmdRecover {
     /// Path to validator keys. Default: `$TYCHO_HOME/validator_keys.json`
     #[clap(long)]
     sign: Option<PathBuf>,
+
+    /// Skip waiting for the message delivery.
+    #[clap(long)]
+    no_wait: bool,
 }
 
 impl CmdRecover {
@@ -197,7 +240,7 @@ impl CmdRecover {
             let config = client.get_blockchain_config().await?;
 
             // Find reward
-            let elector_data = client.get_elector_data().await?;
+            let (_, elector_data) = client.get_elector_data().await?;
             let Some(to_recover) = elector_data.credits.get(&wallet.address.address) else {
                 return print_json(RecoverStatus::NoReward);
             };
@@ -208,7 +251,7 @@ impl CmdRecover {
                 .build()?;
 
             // Send recover message
-            let sent = wallet
+            let message = wallet
                 .transfer(
                     &ELECTOR_ADDR,
                     Amount::Exact(ONE_CC),
@@ -218,10 +261,13 @@ impl CmdRecover {
                 )
                 .await?;
 
+            if !self.no_wait {
+                wallet.wait_delivered(&message).await?;
+            }
+
             // Done
             print_json(RecoverStatus::Recovered {
-                message_hash: sent.hash,
-                message_expire_at: sent.expire_at,
+                message,
                 amount: to_recover.into_inner(),
             })
         })
@@ -233,8 +279,7 @@ impl CmdRecover {
 enum RecoverStatus {
     NoReward,
     Recovered {
-        message_hash: HashBytes,
-        message_expire_at: u32,
+        message: SentMessage,
         #[serde(with = "serde_helpers::string")]
         amount: u128,
     },
@@ -269,6 +314,10 @@ struct CmdWithdraw {
     /// Withdrawal message payload as a base64-encoded BOC.
     #[clap(short, long)]
     payload: Option<String>,
+
+    /// Skip waiting for the message delivery.
+    #[clap(long)]
+    no_wait: bool,
 }
 
 impl CmdWithdraw {
@@ -292,7 +341,7 @@ impl CmdWithdraw {
                 None => Amount::All,
                 Some(amount) => Amount::Exact(amount),
             };
-            let sent = wallet
+            let message = wallet
                 .transfer(
                     &self.dest,
                     amount,
@@ -302,9 +351,12 @@ impl CmdWithdraw {
                 )
                 .await?;
 
+            if !self.no_wait {
+                wallet.wait_delivered(&message).await?;
+            }
+
             print_json(serde_json::json!({
-                "message_hash": sent.hash,
-                "message_expire_at": sent.expire_at,
+                "message": message,
             }))
         })
     }
@@ -320,7 +372,7 @@ struct CmdGetState {
 impl CmdGetState {
     fn run(self, args: BaseArgs) -> Result<()> {
         self.control.rt(args, move |client| async move {
-            let elector_data = client.get_elector_data().await?;
+            let (_, elector_data) = client.get_elector_data().await?;
             print_json(elector_data)
         })
     }
@@ -361,13 +413,12 @@ impl Wallet {
         payload: Cell,
         signature_id: Option<i32>,
     ) -> Result<SentMessage> {
-        let account_state = self
-            .client
-            .get_account_state(&self.address)
-            .await?
-            .context("wallet account does not exist")?;
+        let account = {
+            let (_, account) = self.client.get_account_state(&self.address).await?;
+            account.context("wallet account does not exist")?
+        };
 
-        let init = match account_state.state {
+        let init = match account.state {
             AccountState::Active(_) => None,
             AccountState::Uninit => Some(wallet::make_state_init(
                 &ed25519::PublicKey::from_bytes(self.public.to_bytes()).unwrap(),
@@ -380,7 +431,7 @@ impl Wallet {
             Amount::All => (0, wallet::MSG_FLAGS_SEND_ALL),
         };
         anyhow::ensure!(
-            account_state.balance.tokens.into_inner() >= value,
+            account.balance.tokens.into_inner() >= value,
             "insufficient balance"
         );
 
@@ -422,9 +473,49 @@ impl Wallet {
         self.client.broadcast_message(message_cell.as_ref()).await?;
 
         Ok(SentMessage {
+            timestamp: now_ms,
             hash: *message_cell.repr_hash(),
             expire_at,
         })
+    }
+
+    // TODO: Use some better way to determine the delivery status.
+    async fn wait_delivered(&self, message: &SentMessage) -> Result<()> {
+        let mut interval = tokio::time::interval(Duration::from_secs(1));
+        loop {
+            interval.tick().await;
+
+            let (timings, account) = self.client.get_account_state(&self.address).await?;
+
+            'check: {
+                let Some(account) = account else {
+                    // Account not found
+                    break 'check;
+                };
+
+                let AccountState::Active(StateInit {
+                    data: Some(data), ..
+                }) = account.state
+                else {
+                    // Only deployed account accepts messages
+                    break 'check;
+                };
+
+                let Ok(data) = data.as_slice() else {
+                    break 'check;
+                };
+
+                if let Ok(stored_timestamp) = data.get_u64(256) {
+                    if stored_timestamp >= message.timestamp {
+                        return Ok(());
+                    }
+                }
+            }
+
+            if timings.gen_utime > message.expire_at {
+                anyhow::bail!("message was not delivered");
+            }
+        }
     }
 }
 
@@ -436,6 +527,8 @@ enum Amount {
 
 #[derive(Debug, Clone, Copy, Serialize)]
 struct SentMessage {
+    #[serde(skip)]
+    timestamp: u64,
     hash: HashBytes,
     expire_at: u32,
 }
@@ -538,22 +631,23 @@ struct Client {
 }
 
 impl Client {
-    async fn get_elector_data(&self) -> Result<elector::data::PartialElectorData> {
+    async fn get_elector_data(&self) -> Result<(Timings, elector::data::PartialElectorData)> {
         static ELECTOR_ABI: OnceLock<AbiType> = OnceLock::new();
 
-        let account_state = self
-            .get_account_state(&ELECTOR_ADDR)
-            .await?
-            .context("elector contract not found")?;
+        let (timings, account) = self.get_account_state(&ELECTOR_ADDR).await?;
+        let account = account.context("elector account not found")?;
 
-        let elector_data = match account_state.state {
+        let elector_data = match account.state {
             AccountState::Active(state) => state.data.context("elector data is empty")?,
             _ => anyhow::bail!("invalid elector state"),
         };
 
         let abi_type = ELECTOR_ABI.get_or_init(elector::data::PartialElectorData::abi_type);
-        AbiValue::load_partial(&abi_type, AbiVersion::V2_1, &mut elector_data.as_slice()?)
-            .and_then(elector::data::PartialElectorData::from_abi)
+        let data =
+            AbiValue::load_partial(&abi_type, AbiVersion::V2_1, &mut elector_data.as_slice()?)
+                .and_then(elector::data::PartialElectorData::from_abi)?;
+
+        Ok((timings, data))
     }
 
     async fn get_blockchain_config(&self) -> Result<ParsedBlockchainConfig> {
@@ -569,18 +663,34 @@ impl Client {
         }
     }
 
-    async fn get_account_state(&self, addr: &StdAddr) -> Result<Option<Account>> {
+    async fn get_account_state(&self, addr: &StdAddr) -> Result<(Timings, Option<Account>)> {
         match self.inner.as_ref() {
             ClientImpl::Jrpc(rpc) => match rpc.get_account(addr).await? {
-                jrpc_client::AccountStateResponse::Exists { account, .. } => Ok(Some(*account)),
-                jrpc_client::AccountStateResponse::NotExists { .. } => Ok(None),
+                jrpc_client::AccountStateResponse::Exists {
+                    account, timings, ..
+                } => {
+                    let timings = Timings {
+                        gen_utime: timings.gen_utime,
+                    };
+                    Ok((timings, Some(*account)))
+                }
+                jrpc_client::AccountStateResponse::NotExists { timings } => {
+                    let timings = Timings {
+                        gen_utime: timings.gen_utime,
+                    };
+                    Ok((timings, None))
+                }
                 jrpc_client::AccountStateResponse::Unchanged { .. } => {
                     anyhow::bail!("unexpected response")
                 }
             },
             ClientImpl::Control(control) => {
                 let res = control.get_account_state(addr).await?.parse()?;
-                res.state.load_account().map_err(Into::into)
+                let account = res.state.load_account()?;
+                let timings = Timings {
+                    gen_utime: res.gen_utime,
+                };
+                Ok((timings, account))
             }
         }
     }
@@ -601,11 +711,14 @@ enum ClientImpl {
     Jrpc(JrpcClient),
 }
 
+struct Timings {
+    gen_utime: u32,
+}
+
 struct ParsedBlockchainConfig {
     signature_id: Option<i32>,
-    #[allow(unused)]
-    election_timings: ElectionTimings,
     stake_params: ValidatorStakeParams,
+    config: BlockchainConfig,
 }
 
 impl ParsedBlockchainConfig {
@@ -616,13 +729,12 @@ impl ParsedBlockchainConfig {
             .contains(GlobalCapability::CapSignatureWithId)
             .then_some(global_id);
 
-        let election_timings = config.get_election_timings()?;
         let stake_params = config.get_validator_stake_params()?;
 
         Ok(Self {
             signature_id,
-            election_timings,
             stake_params,
+            config,
         })
     }
 
@@ -639,11 +751,53 @@ impl ParsedBlockchainConfig {
     }
 
     fn check_max_factor(&self, max_factor: u32) -> Result<()> {
-        anyhow::ensure!(max_factor <= (1 << 16), "max factor is too small");
+        anyhow::ensure!(max_factor >= (1 << 16), "max factor is too small");
         anyhow::ensure!(
-            max_factor >= self.stake_params.max_stake_factor,
+            max_factor <= self.stake_params.max_stake_factor,
             "max factor is too big"
         );
         Ok(())
     }
+
+    fn compute_elections_timeline(&self, now: u32) -> Result<Timeline> {
+        let timings = self.config.get_election_timings()?;
+        let current_vset = self.config.get_current_validator_set()?;
+
+        let round_end = current_vset.utime_until;
+        let elections_start = round_end.saturating_sub(timings.elections_start_before);
+        let elections_end = round_end.saturating_sub(timings.elections_end_before);
+
+        if let Some(until_elections) = elections_start.checked_sub(now) {
+            return Ok(Timeline::BeforeElections {
+                until_elections_start: until_elections,
+            });
+        }
+
+        if let Some(until_end) = elections_end.checked_sub(now) {
+            return Ok(Timeline::Elections {
+                since_elections_start: now.saturating_sub(elections_start),
+                until_elections_end: until_end,
+                elections_end,
+            });
+        }
+
+        Ok(Timeline::AfterElections {
+            until_round_end: round_end.saturating_sub(now),
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Timeline {
+    BeforeElections {
+        until_elections_start: u32,
+    },
+    Elections {
+        since_elections_start: u32,
+        until_elections_end: u32,
+        elections_end: u32,
+    },
+    AfterElections {
+        until_round_end: u32,
+    },
 }
