ed353d709a5516324713a87ccdf814bc6bc27583
Kirill Mikheev <mikheyev.k@gmail.com>
2024-12-04 13:10:29 +0300

    fix(consensus): make `Threshold` show count while subscribed

--- consensus/src/dag/threshold.rs
+++ consensus/src/dag/threshold.rs
@@ -1,3 +1,6 @@
+use std::sync::atomic;
+use std::sync::atomic::AtomicUsize;
+
 use tokio::sync::Semaphore;
 
 use crate::models::{PeerCount, Round};
@@ -6,6 +9,7 @@ pub struct Threshold {
     round: Round,
     peer_count: PeerCount,
     semaphore: Semaphore,
+    count: AtomicUsize,
 }
 
 impl Threshold {
@@ -14,14 +18,16 @@ impl Threshold {
             round,
             peer_count,
             semaphore: Semaphore::new(0),
+            count: AtomicUsize::new(0),
         }
     }
     pub fn add(&self) {
         self.semaphore.add_permits(1);
+        self.count.fetch_add(1, atomic::Ordering::Relaxed);
     }
     /// used in between calls to [`Self::reached`]
     pub fn count(&self) -> usize {
-        self.semaphore.available_permits()
+        self.count.load(atomic::Ordering::Relaxed)
     }
     pub async fn reached(&self) {
         // drop of permits return them to semaphore for other waiters
@@ -37,5 +43,38 @@ impl Threshold {
                 futures_util::future::pending::<()>().await;
             }
         };
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+    use std::time::Duration;
+
+    use super::*;
+    use crate::dag::threshold::Threshold;
+    use crate::models::PeerCount;
+
+    #[tokio::test]
+    async fn test() -> anyhow::Result<()> {
+        let total_peers = 10;
+
+        let thresh = Arc::new(Threshold::new(Round(0), PeerCount::try_from(total_peers)?));
+        let thresh2 = thresh.clone();
+
+        let handle = tokio::spawn(async move {
+            thresh2.reached().await;
+            println!("reached");
+        });
+
+        for i in 1..total_peers {
+            tokio::time::sleep(Duration::from_millis(10)).await;
+            thresh.add();
+            println!("count {}", thresh.count());
+            assert_eq!(i, thresh.count(), "must return count");
+        }
+
+        handle.await?;
+        Ok(())
     }
 }
