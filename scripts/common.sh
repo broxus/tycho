ea5db5b42e6e066664851925a3d5fe813a15a040
Ivan Kalinin <i.kalinin@dexpa.io>
2024-12-09 15:23:39 +0100

    feat(scripts): set `CC` and `CXX` to `clang` while building the node

--- scripts/common.sh
+++ scripts/common.sh
@@ -5,3 +5,31 @@ function is_number {
         return 1
     fi
 }
+
+function set_clang_env {
+    local clang_version="$1"
+    if [ -z "$clang_version" ]; then
+        echo "ERROR: Expected clang versrion for 'set_clang_env'."
+        exit 1
+    fi
+
+    if [ -z "$2" ] || [ "$2" == "require" ]; then
+        local require="$2"
+    else
+        echo "ERROR: Expected optional 'require' flag for 'set_clang_env'."
+        exit 1
+    fi
+
+    local cc_path=$(command -v "clang-${clang_version}" 2>&1 || true)
+    local cxx_path=$(command -v "clang++-${clang_version}" 2>&1 || true)
+
+    if [ ! -z "$cc_path" ] && [ ! -z "$cxx_path" ]; then
+        export CC="$cc_path"
+        export CXX="$cxx_path"
+    elif [ -z "$require" ]; then
+        echo "WARN: Clang ${clang_version} not found, fallback to default build."
+    else
+        echo "ERROR: Clang ${clang_version} required but not found."
+        exit 1
+    fi
+}
---
+++ scripts/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -eE
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
+root_dir=$(cd "${script_dir}/../" && pwd -P)
+
+source "${script_dir}/common.sh"
+
+if ! command -v cargo 2>&1 >/dev/null
+then
+    cat << EOF
+ERROR: \`cargo\` could not be found. You need to install the Rust compiler first:
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+And add its environment variables:
+
+source ~/.cargo/env
+
+EOF
+    exit 1
+fi
+
+set_clang_env 19
+cargo install --path ./cli --locked
+
+cat << EOF
+Node installed successfully. Run the following to configure it:
+
+tycho init --systemd --global-config https://testnet.tychoprotocol.com/global-config.json \\
+  --validator --stake 500000
+
+Where
+  --global-config: file path or URL to the network global config
+  --stake: stake value per round
+EOF
