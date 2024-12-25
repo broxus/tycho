bb01d767265958ae90da96ae5b9c34722c845a33
Kirill Mikheev <mikheyev.k@gmail.com>
2024-12-25 20:18:55 +0300

    refactor(consensus): restart
    * split download and restore futures
    * do not produce alternative point if db was removed

--- consensus/src/models/point_status.rs
+++ consensus/src/models/point_status.rs
@@ -3,7 +3,105 @@ use std::fmt::{Debug, Display, Formatter};
 use tycho_network::PeerId;
 use tycho_storage::point_status::{AnchorFlags, StatusFlags};
 
-use crate::effects::AltFormat;
+use crate::effects::{AltFmt, AltFormat};
+use crate::models::{Digest, PointId, PointInfo, PrevPointProof, Round};
+
+pub enum PointRestore {
+    /// non-terminal status can be found in DB if point task was aborted before validation;
+    /// Note: must repeat `verify()`, as this also represents an error during merge of DB statuses
+    Exists(PointInfo, Option<PrevPointProof>),
+    Valid(PointInfo, PointStatusValid),
+    Invalid(PointInfo, PointStatusInvalid),
+    IllFormed(PointId, PointStatusIllFormed),
+    NotFound(Round, Digest, PointStatusNotFound),
+}
+impl PointRestore {
+    /// required partial order: resolved + valid, resolved, valid, no flags, no status
+    pub fn restore_order_asc(&self) -> u8 {
+        /// greater value for greater priority
+        fn order_desc<T: PointStatus>(status: &T) -> u8 {
+            let mut priority = 1;
+            priority |= (status.is_first_resolved() as u8) << 7;
+            priority |= (status.is_first_valid() as u8) << 6;
+            priority
+        }
+        let desc = match self {
+            PointRestore::Exists(_, _) => 0,
+            PointRestore::Valid(_, status) => order_desc(status),
+            PointRestore::Invalid(_, status) => order_desc(status),
+            PointRestore::IllFormed(_, status) => order_desc(status),
+            PointRestore::NotFound(_, _, status) => order_desc(status),
+        };
+        !desc // invert priority for ascending order
+    }
+    pub fn round(&self) -> Round {
+        match self {
+            Self::Exists(info, _) | Self::Valid(info, _) | Self::Invalid(info, _) => info.round(),
+            Self::IllFormed(id, _) => id.round,
+            Self::NotFound(round, _, _) => *round,
+        }
+    }
+    pub fn author(&self) -> &PeerId {
+        match self {
+            Self::Exists(info, _) | Self::Valid(info, _) | Self::Invalid(info, _) => {
+                &info.data().author
+            }
+            Self::IllFormed(id, _) => &id.author,
+            Self::NotFound(_, _, status) => &status.author,
+        }
+    }
+    pub fn digest(&self) -> &Digest {
+        match self {
+            Self::Exists(info, _) | Self::Valid(info, _) | Self::Invalid(info, _) => info.digest(),
+            Self::IllFormed(id, _) => &id.digest,
+            Self::NotFound(_, digest, _) => digest,
+        }
+    }
+    pub fn id(&self) -> PointId {
+        PointId {
+            author: *self.author(),
+            round: self.round(),
+            digest: *self.digest(),
+        }
+    }
+}
+
+impl AltFormat for PointRestore {}
+impl Debug for AltFmt<'_, PointRestore> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let inner = AltFormat::unpack(self);
+        write!(f, "Restore {{ {:?} ", inner.id().alt())?;
+        match inner {
+            PointRestore::Exists(_, Some(prev)) => {
+                write!(f, "Exists prev # {}", prev.digest.alt())?;
+            }
+            PointRestore::Exists(_, None) => {
+                f.write_str("Exists prev # None")?;
+            }
+            PointRestore::Valid(_, status) => {
+                write!(f, "{status}")?;
+            }
+            PointRestore::Invalid(_, status) => {
+                write!(f, "{status}")?;
+            }
+            PointRestore::IllFormed(_, status) => {
+                write!(f, "{status}")?;
+            }
+            PointRestore::NotFound(_, _, status) => {
+                let mut tuple = f.debug_tuple("NotFound");
+                if status.is_first_resolved {
+                    tuple.field(&"first resolved");
+                }
+                if status.is_certified {
+                    tuple.field(&"certified");
+                }
+                // author is shown in point id
+                tuple.finish()?;
+            }
+        }
+        f.write_str(" }")
+    }
+}
 
 #[derive(Debug)]
 pub enum PointStatusStored {
