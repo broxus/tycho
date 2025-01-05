d06a43fb22522707efb354ee0777c86b2e3a57a3
Maksim Greshniakov <infiltrator53@gmail.com>
2025-01-05 01:27:16 +0100

    feature(collator): int queue sync partition router

--- collator/src/manager/mod.rs
+++ collator/src/manager/mod.rs
@@ -1289,7 +1289,6 @@ where
                         queue_diff_with_messages,
                         *queue_diff_stuff.diff_hash(),
                         prev_block_id,
-                        queue_diff_stuff.diff().max_message,
                     ));
 
                     let prev_ids_info = block_stuff.construct_prev_id()?;
@@ -1302,7 +1301,7 @@ where
         }
 
         // apply required previous queue diffs
-        while let Some((diff, diff_hash, block_id, max_message_key)) = prev_queue_diffs.pop() {
+        while let Some((diff, diff_hash, block_id)) = prev_queue_diffs.pop() {
             let statistics = (&diff, block_id.shard).into();
             self.mq_adapter
                 .apply_diff(diff, block_id.as_short_id(), &diff_hash, statistics)?;
