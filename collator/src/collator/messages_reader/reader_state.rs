80beff07c62723708bf892cbb59cf055b83bbc99
Vitaly Terekhov <va.tehb@gmail.com>
2025-01-13 14:17:25 +0000

    feat(collator): split externals ProcessedUpto by partitions, updated messages read flow
    - read and collect existing internals and externals;
    - when existing internals finished, collect read externals from buffer without reading more
    - when all externals collected from buffer, read and collect remaning externals and new messages

--- collator/src/collator/messages_reader/reader_state.rs
+++ collator/src/collator/messages_reader/reader_state.rs
@@ -1,16 +1,20 @@
 use std::collections::BTreeMap;
 
+use anyhow::{Context, Result};
 use everscale_types::models::ShardIdent;
 use tycho_block_util::queue::QueueKey;
 
 use super::super::messages_buffer::MessagesBuffer;
+use crate::collator::messages_buffer::{
+    BufferFillStateByCount, BufferFillStateBySlots, MessagesBufferLimits,
+};
 use crate::mempool::MempoolAnchorId;
 use crate::types::processed_upto::{
     BlockSeqno, ExternalsProcessedUptoStuff, ExternalsRangeInfo, InternalsProcessedUptoStuff,
-    InternalsRangeStuff, Lt, PartitionId, PartitionProcessedUptoStuff, ProcessedUptoInfoStuff,
+    InternalsRangeStuff, Lt, PartitionId, ProcessedUptoInfoStuff, ProcessedUptoPartitionStuff,
     ShardRangeInfo,
 };
-use crate::types::ProcessedTo;
+use crate::types::{DebugIter, ProcessedTo};
 
 //=========
 // READER STATE
@@ -24,84 +28,99 @@ pub struct ReaderState {
 
 impl ReaderState {
     pub fn new(processed_upto: &ProcessedUptoInfoStuff) -> Self {
-        Self {
-            externals: ExternalsReaderState {
-                curr_processed_offset: 0,
-                processed_to: processed_upto.externals.processed_to.into(),
-                ranges: processed_upto
-                    .externals
+        let mut ext_reader_state = ExternalsReaderState::default();
+        for (par_id, par) in &processed_upto.partitions {
+            ext_reader_state
+                .by_partitions
+                .insert(*par_id, ExternalsReaderStateByPartition {
+                    processed_to: par.externals.processed_to.into(),
+                    curr_processed_offset: 0,
+                });
+            for (seqno, range_info) in &par.externals.ranges {
+                ext_reader_state
                     .ranges
-                    .iter()
-                    .map(|(k, v)| (*k, v.into()))
-                    .collect(),
-                last_read_to_anchor_chain_time: None,
-            },
+                    .entry(*seqno)
+                    .and_modify(|r| {
+                        r.by_partitions.insert(*par_id, range_info.into());
+                    })
+                    .or_insert(ExternalsRangeReaderState {
+                        range: range_info.into(),
+                        by_partitions: [(*par_id, range_info.into())].into(),
+                    });
+            }
+        }
+        Self {
             internals: InternalsReaderState {
                 partitions: processed_upto
-                    .internals
                     .partitions
                     .iter()
-                    .map(|(k, v)| (*k, v.into()))
+                    .map(|(k, v)| (*k, (&v.internals).into()))
                     .collect(),
             },
+            externals: ext_reader_state,
         }
     }
 
     pub fn get_updated_processed_upto(&self) -> ProcessedUptoInfoStuff {
-        ProcessedUptoInfoStuff {
-            externals: ExternalsProcessedUptoStuff {
-                processed_to: self.externals.processed_to.into(),
-                ranges: self
-                    .externals
-                    .ranges
-                    .iter()
-                    .map(|(k, v)| (*k, v.into()))
-                    .collect(),
-            },
-            internals: InternalsProcessedUptoStuff {
-                partitions: self
-                    .internals
-                    .partitions
-                    .iter()
-                    .map(|(k, v)| (*k, v.into()))
-                    .collect(),
-            },
+        let mut processed_upto = ProcessedUptoInfoStuff::default();
+        for (par_id, par) in &self.internals.partitions {
+            let ext_reader_state_by_partition =
+                self.externals.get_state_by_partition(par_id).unwrap();
+            processed_upto
+                .partitions
+                .insert(*par_id, ProcessedUptoPartitionStuff {
+                    externals: ExternalsProcessedUptoStuff {
+                        processed_to: ext_reader_state_by_partition.processed_to.into(),
+                        ranges: self
+                            .externals
+                            .ranges
+                            .iter()
+                            .map(|(k, v)| {
+                                let ext_range_reader_state_by_partition =
+                                    v.get_state_by_partition(par_id).unwrap();
+                                (*k, (&v.range, ext_range_reader_state_by_partition).into())
+                            })
+                            .collect(),
+                    },
+                    internals: par.into(),
+                });
         }
+        processed_upto
     }
 
     pub fn check_has_non_zero_processed_offset(&self) -> bool {
-        let check_externals = self
-            .externals
-            .ranges
-            .iter()
-            .any(|(_, r_s)| r_s.processed_offset > 0);
-
-        if check_externals {
-            return check_externals;
-        }
-
         let check_internals = self
             .internals
             .partitions
-            .iter()
-            .any(|(_, p_s)| p_s.ranges.iter().any(|(_, r_s)| r_s.processed_offset > 0));
+            .values()
+            .any(|par| par.ranges.values().any(|r| r.processed_offset > 0));
+        if check_internals {
+            return check_internals;
+        }
 
-        check_internals
-    }
-
-    pub fn count_messages_in_buffers(&self) -> usize {
-        let ext_count = self
-            .externals
+        self.externals
             .ranges
             .values()
-            .fold(0, |acc, range| acc + range.buffer.msgs_count());
-        let int_count = self.internals.partitions.values().fold(0, |p_acc, p| {
-            p_acc
-                + p.ranges
-                    .values()
-                    .fold(0, |r_acc, r| r_acc + r.buffer.msgs_count())
-        });
-        ext_count + int_count
+            .any(|r| r.by_partitions.values().any(|par| par.processed_offset > 0))
+    }
+
+    pub fn has_messages_in_buffers(&self) -> bool {
+        self.has_internals_in_buffers() || self.has_externals_in_buffers()
+    }
+
+    pub fn has_internals_in_buffers(&self) -> bool {
+        self.internals
+            .partitions
+            .values()
+            .any(|par| par.ranges.values().any(|r| r.buffer.msgs_count() > 0))
+    }
+
+    pub fn has_externals_in_buffers(&self) -> bool {
+        self.externals.ranges.values().any(|r| {
+            r.by_partitions
+                .values()
+                .any(|par| par.buffer.msgs_count() > 0)
+        })
     }
 }
 
@@ -112,26 +131,83 @@ pub struct ExternalsReaderState {
     /// in any other way.
     /// We need this for not to get messages for account `A` from range `2`
     /// when we still have messages for account `A` in range `1`.
+    ///
+    /// Ranges will be extracted during collation process.
+    /// Should access them only before collation and after reader finalization.
     pub ranges: BTreeMap<BlockSeqno, ExternalsRangeReaderState>,
 
-    /// When all messages from the range (and from all its prev ranges)
-    /// are processed we can update `processed_to` from this range.
-    pub processed_to: ExternalKey,
-
-    /// Actual current processed offset
-    /// during the messages reading.
-    pub curr_processed_offset: u32,
+    /// Partition related externals reader state
+    pub by_partitions: BTreeMap<PartitionId, ExternalsReaderStateByPartition>,
 
     /// last read to anchor chain time
     pub last_read_to_anchor_chain_time: Option<u64>,
 }
 
-#[derive(Default)]
-pub struct ExternalsRangeReaderState {
-    /// Buffer to store external messages
-    /// before collect them to the next execution group
-    pub buffer: MessagesBuffer,
+impl ExternalsReaderState {
+    pub fn get_state_by_partition_mut(
+        &mut self,
+        par_id: &PartitionId,
+    ) -> Result<&mut ExternalsReaderStateByPartition> {
+        self.by_partitions
+            .get_mut(par_id)
+            .with_context(|| format!("externals reader state not exists for partition {}", par_id))
+    }
 
+    pub fn get_state_by_partition(
+        &self,
+        par_id: &PartitionId,
+    ) -> Result<&ExternalsReaderStateByPartition> {
+        self.by_partitions
+            .get(par_id)
+            .with_context(|| format!("externals reader state not exists for partition {}", par_id))
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct ExternalsReaderStateByPartition {
+    /// The last processed external message from all ranges
+    pub processed_to: ExternalKey,
+
+    /// Actual current processed offset
+    /// during the messages reading.
+    pub curr_processed_offset: u32,
+}
+
+pub struct ExternalsRangeReaderState {
+    /// Range info
+    pub range: ExternalsReaderRange,
+    /// Partition related externals range reader state
+    pub by_partitions: BTreeMap<PartitionId, ExternalsRangeReaderStateByPartition>,
+}
+
+impl ExternalsRangeReaderState {
+    pub fn get_state_by_partition_mut(
+        &mut self,
+        par_id: &PartitionId,
+    ) -> Result<&mut ExternalsRangeReaderStateByPartition> {
+        self.by_partitions.get_mut(par_id).with_context(|| {
+            format!(
+                "externals range reader state not exists for partition {}",
+                par_id
+            )
+        })
+    }
+
+    pub fn get_state_by_partition(
+        &self,
+        par_id: &PartitionId,
+    ) -> Result<&ExternalsRangeReaderStateByPartition> {
+        self.by_partitions.get(par_id).with_context(|| {
+            format!(
+                "externals range reader state not exists for partition {}",
+                par_id
+            )
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct ExternalsReaderRange {
     pub from: ExternalKey,
     pub to: ExternalKey,
 
@@ -139,33 +215,62 @@ pub struct ExternalsRangeReaderState {
 
     /// Chain time of the block during whose collation the range was read
     pub chain_time: u64,
-
-    /// How many times external messages were collected from the ranges buffers.
-    /// Every range contains offset that was reached when range was the last.
-    /// So the current last range contains the actual offset.
-    pub processed_offset: u32,
 }
 
-impl From<&ExternalsRangeInfo> for ExternalsRangeReaderState {
+impl From<&ExternalsRangeInfo> for ExternalsReaderRange {
     fn from(value: &ExternalsRangeInfo) -> Self {
         Self {
-            buffer: Default::default(),
             from: value.from.into(),
             to: value.to.into(),
             // on init current position is on the from
             current_position: value.from.into(),
             chain_time: value.chain_time,
+        }
+    }
+}
+
+pub struct ExternalsRangeReaderStateByPartition {
+    /// Buffer to store external messages
+    /// before collect them to the next execution group
+    pub buffer: MessagesBuffer,
+    /// Skip offset before collecting messages from this range.
+    /// Because we should collect from others.
+    pub skip_offset: u32,
+    /// How many times externals messages were collected from all ranges.
+    /// Every range contains offset that was reached when range was the last.
+    /// So the current last range contains the actual offset.
+    pub processed_offset: u32,
+}
+
+impl ExternalsRangeReaderStateByPartition {
+    pub fn check_buffer_fill_state(
+        &self,
+        buffer_limits: &MessagesBufferLimits,
+    ) -> (BufferFillStateByCount, BufferFillStateBySlots) {
+        self.buffer.check_is_filled(buffer_limits)
+    }
+}
+
+impl From<&ExternalsRangeInfo> for ExternalsRangeReaderStateByPartition {
+    fn from(value: &ExternalsRangeInfo) -> Self {
+        Self {
+            buffer: Default::default(),
+            skip_offset: value.skip_offset,
             processed_offset: value.processed_offset,
         }
     }
 }
-impl From<&ExternalsRangeReaderState> for ExternalsRangeInfo {
-    fn from(value: &ExternalsRangeReaderState) -> Self {
+
+impl From<(&ExternalsReaderRange, &ExternalsRangeReaderStateByPartition)> for ExternalsRangeInfo {
+    fn from(
+        (range, state): (&ExternalsReaderRange, &ExternalsRangeReaderStateByPartition),
+    ) -> Self {
         Self {
-            from: value.from.into(),
-            to: value.to.into(),
-            chain_time: value.chain_time,
-            processed_offset: value.processed_offset,
+            from: range.from.into(),
+            to: range.to.into(),
+            chain_time: range.chain_time,
+            skip_offset: state.skip_offset,
+            processed_offset: state.processed_offset,
         }
     }
 }
@@ -173,17 +278,38 @@ impl From<&ExternalsRangeReaderState> for ExternalsRangeInfo {
 pub struct DebugExternalsRangeReaderState<'a>(pub &'a ExternalsRangeReaderState);
 impl std::fmt::Debug for DebugExternalsRangeReaderState<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("ExternalsRangeReaderState")
-            .field("from", &self.0.from)
-            .field("to", &self.0.to)
-            .field("current_position", &self.0.current_position)
-            .field("chain_time", &self.0.chain_time)
-            .field("processed_offset", &self.0.processed_offset)
+        f.debug_struct("")
+            .field("range", &self.0.range)
+            .field(
+                "by_partitions",
+                &DebugIter(
+                    self.0
+                        .by_partitions
+                        .iter()
+                        .map(|(par_id, par)| (par_id, DisplayRangeReaderStateByPartition(par))),
+                ),
+            )
             .finish()
     }
 }
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct DisplayRangeReaderStateByPartition<'a>(pub &'a ExternalsRangeReaderStateByPartition);
+impl std::fmt::Debug for DisplayRangeReaderStateByPartition<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(self, f)
+    }
+}
+impl std::fmt::Display for DisplayRangeReaderStateByPartition<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("")
+            .field("skip_offset", &self.0.skip_offset)
+            .field("processed_offset", &self.0.skip_offset)
+            .finish()
+    }
+}
+
+// TODO: msgs-v3: implement simplified Debug
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ExternalKey {
     pub anchor_id: MempoolAnchorId,
     pub msgs_offset: u64,
@@ -205,7 +331,7 @@ impl From<ExternalKey> for (MempoolAnchorId, u64) {
 
 #[derive(Default)]
 pub struct InternalsReaderState {
-    pub partitions: BTreeMap<PartitionId, PartitionReaderState>,
+    pub partitions: BTreeMap<PartitionId, InternalsPartitionReaderState>,
 }
 
 impl InternalsReaderState {
@@ -224,17 +350,20 @@ impl InternalsReaderState {
 }
 
 #[derive(Default)]
-pub struct PartitionReaderState {
+pub struct InternalsPartitionReaderState {
+    /// Ranges will be extracted during collation process.
+    /// Should access them only before collation and after reader finalization.
     pub ranges: BTreeMap<BlockSeqno, InternalsRangeReaderState>,
-    pub processed_to: BTreeMap<ShardIdent, QueueKey>,
+
+    pub processed_to: ProcessedTo,
 
     /// Actual current processed offset
     /// during the messages reading.
     pub curr_processed_offset: u32,
 }
 
-impl From<&PartitionProcessedUptoStuff> for PartitionReaderState {
-    fn from(value: &PartitionProcessedUptoStuff) -> Self {
+impl From<&InternalsProcessedUptoStuff> for InternalsPartitionReaderState {
+    fn from(value: &InternalsProcessedUptoStuff) -> Self {
         Self {
             curr_processed_offset: 0,
             processed_to: value.processed_to.clone(),
@@ -242,8 +371,8 @@ impl From<&PartitionProcessedUptoStuff> for PartitionReaderState {
         }
     }
 }
-impl From<&PartitionReaderState> for PartitionProcessedUptoStuff {
-    fn from(value: &PartitionReaderState) -> Self {
+impl From<&InternalsPartitionReaderState> for InternalsProcessedUptoStuff {
+    fn from(value: &InternalsPartitionReaderState) -> Self {
         Self {
             processed_to: value.processed_to.clone(),
             ranges: value.ranges.iter().map(|(k, v)| (*k, v.into())).collect(),
@@ -251,7 +380,6 @@ impl From<&PartitionReaderState> for PartitionProcessedUptoStuff {
     }
 }
 
-#[derive(Default)]
 pub struct InternalsRangeReaderState {
     /// Buffer to store messages from the next iterator
     /// for accounts that have messages in the previous iterator
@@ -260,7 +388,10 @@ pub struct InternalsRangeReaderState {
 
     pub shards: BTreeMap<ShardIdent, ShardReaderState>,
 
-    /// How many times internal messages were collected from the ranges buffers.
+    /// Skip offset before collecting messages from this range.
+    /// Because we should collect from others.
+    pub skip_offset: u32,
+    /// How many times internal messages were collected from all ranges.
     /// Every range contains offset that was reached when range was the last.
     /// So the current last range contains the actual offset.
     pub processed_offset: u32,
@@ -270,6 +401,7 @@ impl From<&InternalsRangeStuff> for InternalsRangeReaderState {
     fn from(value: &InternalsRangeStuff) -> Self {
         Self {
             buffer: Default::default(),
+            skip_offset: value.skip_offset,
             processed_offset: value.processed_offset,
             shards: value.shards.iter().map(|(k, v)| (*k, v.into())).collect(),
         }
@@ -278,6 +410,7 @@ impl From<&InternalsRangeStuff> for InternalsRangeReaderState {
 impl From<&InternalsRangeReaderState> for InternalsRangeStuff {
     fn from(value: &InternalsRangeReaderState) -> Self {
         Self {
+            skip_offset: value.skip_offset,
             processed_offset: value.processed_offset,
             shards: value.shards.iter().map(|(k, v)| (*k, v.into())).collect(),
         }
@@ -287,9 +420,18 @@ impl From<&InternalsRangeReaderState> for InternalsRangeStuff {
 pub struct DebugInternalsRangeReaderState<'a>(pub &'a InternalsRangeReaderState);
 impl std::fmt::Debug for DebugInternalsRangeReaderState<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("InternalsRangeReaderState")
+        f.debug_struct("")
+            .field("skip_offset", &self.0.skip_offset)
             .field("processed_offset", &self.0.processed_offset)
-            .field("shards", &self.0.shards)
+            .field(
+                "shards",
+                &DebugIter(
+                    self.0
+                        .shards
+                        .iter()
+                        .map(|(shard_id, r_s)| (shard_id, DisplayShardReaderState(r_s))),
+                ),
+            )
             .finish()
     }
 }
@@ -317,5 +459,21 @@ impl From<&ShardReaderState> for ShardRangeInfo {
             from: value.from,
             to: value.to,
         }
+    }
+}
+
+struct DisplayShardReaderState<'a>(pub &'a ShardReaderState);
+impl std::fmt::Debug for DisplayShardReaderState<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self, f)
+    }
+}
+impl std::fmt::Display for DisplayShardReaderState<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("")
+            .field("from", &self.0.from)
+            .field("to", &self.0.to)
+            .field("current_position", &self.0.current_position)
+            .finish()
     }
 }
