b98bc5b568e91b5459725dd1404095b28c240706
Vitaly Terekhov <va.tehb@gmail.com>
2025-01-26 13:03:48 +0000

    fix(collator): do not update processed_offset on current on refill

--- collator/src/collator/messages_reader/reader_state.rs
+++ collator/src/collator/messages_reader/reader_state.rs
@@ -312,7 +312,7 @@ impl std::fmt::Display for DisplayRangeReaderStateByPartition<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("")
             .field("skip_offset", &self.0.skip_offset)
-            .field("processed_offset", &self.0.skip_offset)
+            .field("processed_offset", &self.0.processed_offset)
             .field("buffer.msgs_count", &self.0.buffer.msgs_count())
             .finish()
     }
