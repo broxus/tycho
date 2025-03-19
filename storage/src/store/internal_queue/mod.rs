fbab87dac5f0e7583b0a04d134f52621d331a349
Vitaly Terekhov <va.tehb@gmail.com>
2025-03-19 08:55:19 +0000

    fixed rustfmt warnings

--- storage/src/store/internal_queue/mod.rs
+++ storage/src/store/internal_queue/mod.rs
@@ -3,11 +3,11 @@
 //! This module provides functionality for storing and working with internal messages
 //! in the queue, including transactions, snapshots, and iterators.
 
-pub mod storage;
-pub mod transaction;
 pub mod iterator;
 pub mod model;
 pub mod snapshot;
+pub mod storage;
+pub mod transaction;
 
 // Constants
 pub const INT_QUEUE_LAST_COMMITTED_MC_BLOCK_ID_KEY: &[u8] = b"last_committed_mc_block_id";
