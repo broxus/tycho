cd5ffe3ea6276b1566b7901943bcfe3707506241
Alexey Pashinov <pashinov93@gmail.com>
2025-04-18 15:30:07 +0200

    WIP: split merkle updates

--- block-util/src/state/mod.rs
+++ block-util/src/state/mod.rs
@@ -1,5 +1,7 @@
 pub use self::min_ref_mc_state::{MinRefMcStateTracker, RefMcStateHandle};
+pub use self::shard_state_data::{split_shard, ShardStateData};
 pub use self::shard_state_stuff::ShardStateStuff;
 
 mod min_ref_mc_state;
+mod shard_state_data;
 mod shard_state_stuff;
