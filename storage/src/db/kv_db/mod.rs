ff7513b0b2e1b95746826e10da8a0946dfe5302c
Alexey Pashinov <pashinov93@gmail.com>
2025-05-16 15:42:22 +0200

    feat(storage): remove old migrations since we need to resync node with a new CellsDb

--- storage/src/db/kv_db/mod.rs
+++ storage/src/db/kv_db/mod.rs
@@ -103,17 +103,13 @@ impl BaseDbExt for BaseDb {
 
 impl WithMigrations for BaseDb {
     const NAME: &'static str = "base";
-    const VERSION: Semver = [0, 0, 3];
+    const VERSION: Semver = [0, 1, 0];
 
     fn register_migrations(
-        migrations: &mut Migrations<Self>,
-        cancelled: CancellationFlag,
+        _migrations: &mut Migrations<Self>,
+        _cancelled: CancellationFlag,
     ) -> Result<(), MigrationError> {
-        migrations.register([0, 0, 1], [0, 0, 2], move |db| {
-            base_migrations::v0_0_1_to_0_0_2(db, cancelled.clone())
-        })?;
-        migrations.register([0, 0, 2], [0, 0, 3], base_migrations::v_0_0_2_to_v_0_0_3)?;
-
+        // TODO: register migrations here
         Ok(())
     }
 }
@@ -129,94 +125,6 @@ weedb::tables! {
         pub package_entries: tables::PackageEntries,
         pub block_data_entries: tables::BlockDataEntries,
         pub block_connections: tables::BlockConnections,
-
-        // tables are empty, but they cannot be deleted because they are in a storage config
-        cells: tables::Cells,
-        _temp_cells: tables::TempCells,
-        _shard_states: tables::ShardStates,
-        _shard_internal_messages: tables::ShardInternalMessagesOld,
-        _int_msg_stats_uncommited: tables::InternalMessageStatsUncommitedOld,
-        _shard_int_msgs_uncommited: tables::ShardInternalMessagesUncommitedOld,
-        _internal_message_stats: tables::InternalMessageStatsOld,
-    }
-}
-
-mod base_migrations {
-    use std::time::Instant;
-
-    use everscale_types::boc::Boc;
-    use tycho_block_util::archive::ArchiveEntryType;
-    use weedb::rocksdb::CompactOptions;
-
-    use super::*;
-    use crate::util::StoredValue;
-
-    pub fn v0_0_1_to_0_0_2(db: &BaseDb, cancelled: CancellationFlag) -> Result<(), MigrationError> {
-        let mut block_data_iter = db.package_entries.raw_iterator();
-        block_data_iter.seek_to_first();
-
-        tracing::info!("stated migrating package entries");
-
-        let started_at = Instant::now();
-        let mut total_processed = 0usize;
-        let mut block_ids_created = 0usize;
-
-        let full_block_ids_cf = &db.full_block_ids.cf();
-        let mut batch = weedb::rocksdb::WriteBatch::default();
-        let mut cancelled = cancelled.debounce(10);
-        loop {
-            let (key, value) = match block_data_iter.item() {
-                Some(item) if !cancelled.check() => item,
-                Some(_) => return Err(MigrationError::Custom(anyhow::anyhow!("cancelled").into())),
-                None => {
-                    block_data_iter.status()?;
-                    break;
-                }
-            };
-
-            'item: {
-                let key = crate::PackageEntryKey::from_slice(key);
-                if key.ty != ArchiveEntryType::Block {
-                    break 'item;
-                }
-
-                let file_hash = Boc::file_hash_blake(value);
-                batch.put_cf(full_block_ids_cf, key.block_id.to_vec(), file_hash);
-                block_ids_created += 1;
-            }
-
-            block_data_iter.next();
-            total_processed += 1;
-        }
-
-        db.rocksdb()
-            .write_opt(batch, db.full_block_ids.write_config())?;
-
-        tracing::info!(
-            elapsed = %humantime::format_duration(started_at.elapsed()),
-            total_processed,
-            block_ids_created,
-            "finished migrating package entries"
-        );
-        Ok(())
-    }
-
-    pub fn v_0_0_2_to_v_0_0_3(db: &BaseDb) -> Result<(), MigrationError> {
-        let mut opts = CompactOptions::default();
-        opts.set_exclusive_manual_compaction(true);
-        let null = Option::<&[u8]>::None;
-
-        let started_at = Instant::now();
-        tracing::info!("started cells compaction");
-        db.cells
-            .db()
-            .compact_range_cf_opt(&db.cells.cf(), null, null, &opts);
-        tracing::info!(
-            elapsed = %humantime::format_duration(started_at.elapsed()),
-            "finished cells compaction"
-        );
-
-        Ok(())
     }
 }
 
@@ -226,16 +134,13 @@ pub type RpcDb = WeeDb<RpcTables>;
 
 impl WithMigrations for RpcDb {
     const NAME: &'static str = "rpc";
-    const VERSION: Semver = [0, 0, 2];
+    const VERSION: Semver = [0, 1, 0];
 
     fn register_migrations(
-        migrations: &mut Migrations<Self>,
-        cancelled: CancellationFlag,
+        _migrations: &mut Migrations<Self>,
+        _cancelled: CancellationFlag,
     ) -> Result<(), MigrationError> {
-        migrations.register([0, 0, 1], [0, 0, 2], move |db| {
-            rpc_migrations::v0_0_1_to_0_0_2(db, cancelled.clone())
-        })?;
-
+        // TODO: register migrations here
         Ok(())
     }
 }
@@ -248,102 +153,6 @@ weedb::tables! {
         pub transactions_by_in_msg: tables::TransactionsByInMsg,
         pub code_hashes: tables::CodeHashes,
         pub code_hashes_by_address: tables::CodeHashesByAddress,
-    }
-}
-
-mod rpc_migrations {
-    use std::time::Instant;
-
-    use everscale_types::boc::{Boc, BocTag};
-    use everscale_types::models::Transaction;
-    use tycho_util::sync::CancellationFlag;
-    use weedb::MigrationError;
-
-    use crate::{RpcDb, TransactionMask};
-
-    pub fn v0_0_1_to_0_0_2(db: &RpcDb, cancelled: CancellationFlag) -> Result<(), MigrationError> {
-        const BATCH_SIZE: usize = 100_000;
-
-        let mut transactions_iter = db.transactions.raw_iterator();
-        transactions_iter.seek_to_first();
-
-        tracing::info!("stated migrating transactions");
-
-        let started_at = Instant::now();
-        let mut total_processed = 0usize;
-
-        let transactions_cf = &db.transactions.cf();
-        let mut batch = weedb::rocksdb::WriteBatch::default();
-        let mut cancelled = cancelled.debounce(10);
-        let mut buffer = Vec::new();
-        loop {
-            let (key, value) = match transactions_iter.item() {
-                Some(item) if !cancelled.check() => item,
-                Some(_) => return Err(MigrationError::Custom(anyhow::anyhow!("cancelled").into())),
-                None => {
-                    transactions_iter.status()?;
-                    break;
-                }
-            };
-
-            if BocTag::from_bytes(value[0..4].try_into().unwrap()).is_none() {
-                // Skip already updated values.
-                transactions_iter.next();
-                continue;
-            }
-
-            let tx_cell = Boc::decode(value).map_err(|_e| {
-                MigrationError::Custom(anyhow::anyhow!("invalid transaction in db").into())
-            })?;
-
-            let tx_hash = tx_cell.repr_hash();
-
-            let tx = tx_cell.parse::<Transaction>().map_err(|_e| {
-                MigrationError::Custom(anyhow::anyhow!("invalid transaction in db").into())
-            })?;
-
-            let mut mask = TransactionMask::empty();
-            if tx.in_msg.is_some() {
-                mask.set(TransactionMask::HAS_MSG_HASH, true);
-            }
-
-            let boc_start = if mask.has_msg_hash() { 65 } else { 33 }; // 1 + 32 + (32)
-
-            buffer.clear();
-            buffer.reserve(boc_start + value.len());
-
-            buffer.push(mask.bits()); // Mask
-            buffer.extend_from_slice(tx_hash.as_slice()); // Tx hash
-
-            if let Some(in_msg) = tx.in_msg {
-                buffer.extend_from_slice(in_msg.repr_hash().as_slice()); // InMsg hash
-            }
-
-            buffer.extend_from_slice(value); // Tx data
-
-            batch.put_cf(transactions_cf, key, &buffer);
-
-            transactions_iter.next();
-            total_processed += 1;
-
-            if total_processed % BATCH_SIZE == 0 {
-                db.rocksdb()
-                    .write_opt(std::mem::take(&mut batch), db.transactions.write_config())?;
-            }
-        }
-
-        if !batch.is_empty() {
-            db.rocksdb()
-                .write_opt(batch, db.transactions.write_config())?;
-        }
-
-        tracing::info!(
-            elapsed = %humantime::format_duration(started_at.elapsed()),
-            total_processed,
-            "finished migrating transactions"
-        );
-
-        Ok(())
     }
 }
 
