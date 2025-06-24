ffc32df3f3348350a2fce1821b75fd4b0465b6ec
Ivan Kalinin <i.kalinin@dexpa.io>
2025-06-24 13:33:48 +0200

    fet(util): extend `tycho-util`

--- util/src/cli/config.rs
+++ util/src/cli/config.rs
@@ -1,10 +1,27 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 #[serde(default)]
 pub struct ThreadPoolConfig {
     pub rayon_threads: usize,
     pub tokio_workers: usize,
+}
+
+impl ThreadPoolConfig {
+    pub fn init_global_rayon_pool(&self) -> Result<(), rayon::ThreadPoolBuildError> {
+        rayon::ThreadPoolBuilder::new()
+            .stack_size(8 * 1024 * 1024)
+            .thread_name(|_| "rayon_worker".to_string())
+            .num_threads(self.rayon_threads)
+            .build_global()
+    }
+
+    pub fn build_tokio_runtime(&self) -> std::io::Result<tokio::runtime::Runtime> {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .worker_threads(self.tokio_workers)
+            .build()
+    }
 }
 
 impl Default for ThreadPoolConfig {
