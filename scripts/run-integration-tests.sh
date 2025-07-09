6cdd1ba2eba4bc58ad973e49bd2b918dd381e6c0
Maksim Greshnyakov <infiltrator53@gmail.com>
2025-07-10 00:05:03 +0200

    test(core): integration test fix

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -3,8 +3,12 @@ set -eE
 
 #cargo test -r --all-targets --all-features --workspace -- --ignored #uncomment this when all crates will compile ˙◠˙
 # for now add tests one by one
-RUST_BACKTRACE=1 cargo test --package tycho-core --profile=release_check --test archives heavy_archives \
-    -- --ignored --exact --nocapture
+RUST_BACKTRACE=1 cargo test \
+  --package tycho-core \
+  --profile=release_check \
+  --features=test \
+  --test archives heavy_archives \
+  -- --ignored --exact --nocapture
 
 #RUST_LIB_BACKTRACE=1 RUST_BACKTRACE=1 cargo test -r --package tycho-storage \
 #    --lib store::shard_state::store_state_raw::test::insert_and_delete_of_several_shards \
