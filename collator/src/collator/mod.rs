96cd894fac9441716b77aea78e908fd3008457fc
Alexey Pashinov <pashinov93@gmail.com>
2025-09-30 15:03:14 +0200

    wait for store state

--- collator/src/collator/mod.rs
+++ collator/src/collator/mod.rs
@@ -23,7 +23,6 @@ use tycho_types::models::*;
 use tycho_util::futures::JoinTask;
 use tycho_util::mem::Reclaimer;
 use tycho_util::metrics::{HistogramGuard, HistogramGuardWithLabels};
-use tycho_util::sync::rayon_run;
 use tycho_util::time::now_millis;
 use types::{AnchorInfo, AnchorsCache, MsgsExecutionParamsStuff};
 
@@ -255,8 +254,6 @@ pub struct CollatorStdImpl {
     /// Refs on states to keep them alive until a Merkle chain is applied
     store_state_refs: VecDeque<Cell>,
 
-    background_store_new_state_tx: tokio::sync::mpsc::UnboundedSender<StateUpdateContext>,
-
     anchors_cache: AnchorsCache,
     block_serializer_cache: BlockSerializerCache,
     stats: CollatorStats,
@@ -300,9 +297,6 @@ impl CollatorStdImpl {
 
         let (working_state_tx, working_state_rx) = oneshot::channel::<Result<Box<WorkingState>>>();
 
-        let (background_store_new_state_tx, mut background_store_new_state_rx) =
-            tokio::sync::mpsc::unbounded_channel::<StateUpdateContext>();
-
         let store_state_refs = VecDeque::with_capacity(config.merkle_chain_limit);
 
         let processor = Self {
@@ -322,7 +316,6 @@ impl CollatorStdImpl {
             }),
             store_new_state_tasks: Default::default(),
             store_state_refs,
-            background_store_new_state_tx,
             anchors_cache: Default::default(),
             block_serializer_cache: BlockSerializerCache::with_capacity(BLOCK_CELL_COUNT_BASELINE),
             stats: Default::default(),
@@ -333,17 +326,6 @@ impl CollatorStdImpl {
             cancel_collation,
             wu_tuner_event_sender,
         };
-
-        // finalize new state store tasks in background
-        tokio::spawn(async move {
-            while let Some(cx) = background_store_new_state_rx.recv().await {
-                if let Err(err) = cx.store_new_state_task.await {
-                    tracing::error!(target: tracing_targets::COLLATOR,
-                        "Error when store new state: {:?}", err,
-                    );
-                }
-            }
-        });
 
         // create dispatcher for own async tasks queue
         let dispatcher =
@@ -639,6 +621,17 @@ impl CollatorStdImpl {
 
         // update collation session info to refer to a correct subset in collated block
         self.collation_session = collation_session;
+
+        // previously wait when all state store tasks finished
+        if !self.store_new_state_tasks.is_empty() {
+            tracing::debug!(target: tracing_targets::COLLATOR,
+                "awaiting when all state store tasks finished...",
+            );
+            for ctx in self.store_new_state_tasks.drain(..) {
+                ctx.store_new_state_task.await?;
+            }
+        }
+
         let mut working_state = if !reset {
             let mut working_state = self.delayed_working_state.wait().await?;
 
@@ -659,82 +652,13 @@ impl CollatorStdImpl {
 
                 // and only for shard collator
                 // update prev states to drop usage tree
-                if !self.shard_id.is_masterchain() && !self.store_new_state_tasks.is_empty() {
-                    // get last store task
-                    let last_task = self.store_new_state_tasks.pop().expect("shouldn't happen");
-
-                    // if it is finished, then we can just reload prev state
-                    if last_task.store_new_state_task.is_finished() {
-                        last_task.store_new_state_task.await?;
-
-                        // and reload pure prev state in the working state
-                        Self::reload_prev_data(
-                            prev_mc_seqno,
-                            &mut working_state,
-                            self.state_node_adapter.clone(),
-                        )
-                        .await?;
-                    } else {
-                        let mut unfinished_tasks = vec![last_task];
-
-                        // Process previous tasks until finding the finished one
-                        while let Some(task) = self.store_new_state_tasks.pop() {
-                            let is_last = self.store_new_state_tasks.is_empty();
-
-                            // collect all unfinished tasks
-                            if !task.store_new_state_task.is_finished()
-                                && unfinished_tasks.len() < self.config.merkle_chain_limit
-                                && !is_last
-                            {
-                                unfinished_tasks.push(task);
-                                continue;
-                            }
-
-                            task.store_new_state_task.await?;
-
-                            let _histogram_apply_merkles = HistogramGuard::begin_with_labels(
-                                "tycho_collator_resume_collation_apply_merkles_time_high",
-                                &labels,
-                            );
-
-                            // load stored state
-                            let mut prev_state = self
-                                .state_node_adapter
-                                .load_state(prev_mc_seqno, &task.block_id)
-                                .await
-                                .context("failed to load prev shard state")?;
-
-                            while let Some(task) = unfinished_tasks.pop() {
-                                let prev_block_id = *prev_state.block_id();
-                                prev_state = rayon_run({
-                                    let block_id = task.block_id;
-                                    let merkle_update = task.state_update.clone();
-                                    move || {
-                                        prev_state.par_make_next_state(
-                                            &block_id,
-                                            &merkle_update,
-                                            Some(5),
-                                        )
-                                    }
-                                })
-                                .await
-                                .with_context(|| {
-                                    format!(
-                                        "failed to apply merkle of block {} to {prev_block_id}",
-                                        task.block_id
-                                    )
-                                })?;
-
-                                // finalize last store task in background
-                                self.background_store_new_state_tx.send(task)?;
-                            }
-
-                            // and update pure prev state in working state
-                            Self::update_prev_data(&mut working_state, prev_state).await?;
-
-                            break;
-                        }
-                    }
+                if !self.shard_id.is_masterchain() && working_state.next_block_id_short.seqno != 0 {
+                    Self::reload_prev_data(
+                        prev_mc_seqno,
+                        &mut working_state,
+                        self.state_node_adapter.clone(),
+                    )
+                    .await?;
                 }
             }
 
@@ -743,21 +667,11 @@ impl CollatorStdImpl {
                 Reclaimer::instance().drop(cell);
             }
 
-            // finalize all remaining state store tasks in background
-            for cx in self.store_new_state_tasks.drain(..) {
-                self.background_store_new_state_tx.send(cx)?;
-            }
-
             working_state
         } else {
             // Drop states
             while let Some(cell) = self.store_state_refs.pop_front() {
                 Reclaimer::instance().drop(cell);
-            }
-
-            // finalize all remaining state store tasks in background
-            for cx in self.store_new_state_tasks.drain(..) {
-                self.background_store_new_state_tx.send(cx)?;
             }
 
             // reset any delayed working state because we will init a new one
