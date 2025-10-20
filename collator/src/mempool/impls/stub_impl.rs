e965a1e0c6af5a632709d2e6419a7535d735f706
serejkaaa512 <5125402@mail.ru>
2025-10-20 11:18:01 +0300

    feat(collator): add dump state tool
    feat(collator): remove test collator manager
    
    feat(collator): fix dump path
    
    feat(collator): rename dump test, fix test stop on received block
    
    feat(collator): improve collation_tests

--- collator/src/mempool/impls/stub_impl.rs
+++ collator/src/mempool/impls/stub_impl.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, VecDeque};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -25,6 +25,7 @@ pub struct MempoolAdapterStubImpl {
     listener: Arc<dyn MempoolEventListener>,
     anchors_cache: Arc<RwLock<BTreeMap<MempoolAnchorId, Arc<MempoolAnchor>>>>,
     sleep_between_anchors: AtomicBool,
+    top_processed_to_anchor: Option<u32>,
 }
 
 impl MempoolAdapterStubImpl {
@@ -32,7 +33,7 @@ impl MempoolAdapterStubImpl {
         listener: Arc<dyn MempoolEventListener>,
         now: Option<u64>,
     ) -> Arc<Self> {
-        Self::with_generator(listener, |a| {
+        Self::with_generator(listener, None, |a| {
             tokio::spawn(Self::stub_externals_generator(a, now));
             Ok(())
         })
@@ -43,7 +44,7 @@ impl MempoolAdapterStubImpl {
         listener: Arc<dyn MempoolEventListener>,
         dir_path: impl AsRef<Path>,
     ) -> Result<Arc<Self>> {
-        Self::with_generator(listener, move |a| {
+        Self::with_generator(listener, None, move |a| {
             let mut paths = std::fs::read_dir(dir_path)?
                 .map(|res| res.map(|e| e.path()))
                 .collect::<Result<Vec<_>, _>>()?;
@@ -54,7 +55,11 @@ impl MempoolAdapterStubImpl {
         })
     }
 
-    fn with_generator<F>(listener: Arc<dyn MempoolEventListener>, start: F) -> Result<Arc<Self>>
+    fn with_generator<F>(
+        listener: Arc<dyn MempoolEventListener>,
+        top_processed_to_anchor: Option<u32>,
+        start: F,
+    ) -> Result<Arc<Self>>
     where
         F: FnOnce(Arc<Self>) -> Result<()>,
     {
@@ -64,6 +69,7 @@ impl MempoolAdapterStubImpl {
             listener,
             anchors_cache: Arc::new(RwLock::new(BTreeMap::new())),
             sleep_between_anchors: AtomicBool::new(true),
+            top_processed_to_anchor,
         };
 
         let adapter = Arc::new(adapter);
@@ -73,6 +79,26 @@ impl MempoolAdapterStubImpl {
         Ok(adapter)
     }
 
+    pub fn with_anchors_from_dump(
+        listener: Arc<dyn MempoolEventListener>,
+        now: Option<u64>,
+        top_processed_to_anchor_mc: u32,
+        top_processed_to_anchor_shards: u32,
+        anchors_path: PathBuf,
+    ) -> Result<Arc<Self>> {
+        Self::with_generator(listener.clone(), Some(top_processed_to_anchor_shards), {
+            move |a| {
+                tokio::spawn(Self::anchors_generator(
+                    a,
+                    anchors_path,
+                    now,
+                    top_processed_to_anchor_mc,
+                ));
+                Ok(())
+            }
+        })
+    }
+
     #[tracing::instrument(skip_all)]
     async fn stub_externals_generator(self: Arc<Self>, now: Option<u64>) {
         tracing::info!(target: tracing_targets::MEMPOOL_ADAPTER, "started");
@@ -80,15 +106,17 @@ impl MempoolAdapterStubImpl {
             tracing::info!(target: tracing_targets::MEMPOOL_ADAPTER, "finished");
         }
 
-        let mut prev_anchor_id = 0;
-        for anchor_id in 1.. {
+        let mut prev_anchor_id = self.top_processed_to_anchor.unwrap_or_default();
+        let start_anchor_id = prev_anchor_id + 1;
+        for anchor_id in start_anchor_id.. {
             if self.sleep_between_anchors.load(Ordering::Acquire) {
                 tokio::time::sleep(make_round_interval() * 4).await;
             } else {
                 tokio::time::sleep(Duration::from_millis(10)).await;
             }
 
-            let mut anchor = make_stub_anchor(anchor_id, prev_anchor_id);
+            let mut anchor =
+                make_stub_anchor(anchor_id, prev_anchor_id, self.top_processed_to_anchor);
             prev_anchor_id = anchor_id;
 
             if let Some(now) = now {
@@ -105,6 +133,104 @@ impl MempoolAdapterStubImpl {
                 chain_time = anchor.chain_time,
                 externals = anchor.externals.len(),
                 "anchor added to cache",
+            );
+
+            self.listener.on_new_anchor(anchor).await.unwrap();
+        }
+    }
+
+    #[tracing::instrument(skip_all)]
+    #[allow(clippy::todo)]
+    async fn anchors_generator(
+        self: Arc<Self>,
+        anchors_path: PathBuf,
+        now: Option<u64>,
+        top_processed_to_anchor_mc: u32,
+    ) {
+        tracing::info!(target: tracing_targets::MEMPOOL_ADAPTER, "started");
+        defer! {
+            tracing::info!(target: tracing_targets::MEMPOOL_ADAPTER, "finished");
+        }
+
+        let mut file_queue: VecDeque<(MempoolAnchorId, u64, PathBuf)> = VecDeque::new();
+        if anchors_path.exists()
+            && let Ok(entries) = std::fs::read_dir(&anchors_path)
+        {
+            for entry in entries.filter_map(Result::ok) {
+                let path = entry.path();
+                if let Some((anchor_id, chain_time)) = path
+                    .file_name()
+                    .and_then(|s| s.to_str())
+                    .and_then(|s| s.strip_prefix("anchor_"))
+                    .and_then(|f| {
+                        let mut split = f.split('_');
+                        split.next().and_then(|anchor_id| {
+                            split.next().map(|chain_time| (anchor_id, chain_time))
+                        })
+                    })
+                {
+                    let anchor_id = anchor_id
+                        .parse::<MempoolAnchorId>()
+                        .expect("Filename should be parseable as u32");
+                    let chain_time: u64 = chain_time
+                        .parse()
+                        .expect("Filename should be parseable as u64");
+                    file_queue.push_back((anchor_id, chain_time, path));
+                }
+            }
+            file_queue.make_contiguous().sort_by_key(|(aid, _, _)| *aid);
+        }
+
+        // fill with empty anchors all from top_processed_to_anchor to top_processed_to_anchor_mc
+        if top_processed_to_anchor_mc > self.top_processed_to_anchor.unwrap_or_default() {
+            let mut prev_anchor_id = self.top_processed_to_anchor.unwrap_or_default();
+            let start_anchor_id = prev_anchor_id;
+            for anchor_id in start_anchor_id..=top_processed_to_anchor_mc {
+                let chain_time = now.unwrap_or_default();
+                let anchor = make_empty_anchor(anchor_id, prev_anchor_id, chain_time + 1336);
+
+                prev_anchor_id = anchor_id;
+
+                self.anchors_cache.write().insert(anchor_id, anchor.clone());
+            }
+        }
+
+        let mut prev_anchor_id = self.top_processed_to_anchor.unwrap_or_default();
+        let start_anchor_id = prev_anchor_id;
+        for anchor_id in start_anchor_id.. {
+            if self.sleep_between_anchors.load(Ordering::Acquire) {
+                tokio::time::sleep(make_round_interval() * 4).await;
+            } else {
+                tokio::time::sleep(Duration::from_millis(10)).await;
+            }
+
+            let anchor =
+                if let Some((file_anchor_id, _chain_time, _file_path)) = file_queue.pop_front() {
+                    if file_anchor_id == anchor_id {
+                        todo!("make anchor from file")
+                    } else {
+                        continue;
+                    }
+                } else {
+                    let read = self.anchors_cache.read();
+                    let chain_time = read
+                        .get(&prev_anchor_id)
+                        .map(|prev_anchor| prev_anchor.chain_time)
+                        .or(now)
+                        .unwrap_or_default();
+                    make_empty_anchor(anchor_id, prev_anchor_id, chain_time + 1336)
+                };
+
+            prev_anchor_id = anchor_id;
+
+            self.anchors_cache.write().insert(anchor_id, anchor.clone());
+
+            tracing::debug!(
+                target: tracing_targets::MEMPOOL_ADAPTER,
+                anchor_id = anchor.id,
+                chain_time = anchor.chain_time,
+                externals = anchor.externals.len(),
+                "anchor added to cache from file",
             );
 
             self.listener.on_new_anchor(anchor).await.unwrap();
@@ -186,12 +312,10 @@ impl MempoolAdapter for MempoolAdapterStubImpl {
         let mut last_attempt_at = None;
         loop {
             let Some(anchor) = self.anchors_cache.read().get(&anchor_id).cloned() else {
-                let last_anchor_id = self
-                    .anchors_cache
-                    .read()
-                    .last_key_value()
-                    .map(|(_, last_anchor)| last_anchor.id)
-                    .unwrap_or_default();
+                let last_anchor_id = self.anchors_cache.read().last_key_value().map_or(
+                    self.top_processed_to_anchor.unwrap_or_default(),
+                    |(_, last_anchor)| last_anchor.id,
+                );
                 if last_anchor_id > anchor_id {
                     return Ok(GetAnchorResult::NotExist);
                 } else {
@@ -275,12 +399,10 @@ impl MempoolAdapter for MempoolAdapterStubImpl {
                 .map(|(_, v)| v.clone());
 
             let Some(anchor) = res else {
-                let last_anchor_id = self
-                    .anchors_cache
-                    .read()
-                    .last_key_value()
-                    .map(|(_, last_anchor)| last_anchor.id)
-                    .unwrap_or_default();
+                let last_anchor_id = self.anchors_cache.read().last_key_value().map_or(
+                    self.top_processed_to_anchor.unwrap_or_default(),
+                    |(_, last_anchor)| last_anchor.id,
+                );
                 let delta = prev_anchor_id.saturating_sub(last_anchor_id);
                 if delta >= 20 {
                     tracing::info!(target: tracing_targets::MEMPOOL_ADAPTER,
@@ -378,8 +500,12 @@ pub(crate) fn make_empty_anchor(
     })
 }
 
-pub(crate) fn make_stub_anchor(id: MempoolAnchorId, prev_id: MempoolAnchorId) -> MempoolAnchor {
-    let chain_time = id as u64 * 1736 % 1000000000;
+pub(crate) fn make_stub_anchor(
+    id: MempoolAnchorId,
+    prev_id: MempoolAnchorId,
+    anchor_id_offset: Option<u32>,
+) -> MempoolAnchor {
+    let chain_time = (id - anchor_id_offset.unwrap_or_default()) as u64 * 1736 % 1000000000;
 
     let externals_count = (chain_time % 10) as u32;
 
