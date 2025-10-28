fab0e465242a75e14da6a34b1b4934f7753f2232
Stanislav Eliseev <s.eliseev@dexpa.io>
2025-10-28 20:00:57 +0100

    feature: slasher v1. Add tests (wip)

---
+++ contracts/wrappers/Slasher.compile.ts
@@ -0,0 +1,9 @@
+import {CompilerConfig} from "@ton/blueprint";
+
+export const compile: CompilerConfig = {
+    lang: "tolk",
+    entrypoint: "src/slasher.tolk",
+    withStackComments: true,
+    withSrcLineComments: true,
+    experimentalOptions: "",
+};
