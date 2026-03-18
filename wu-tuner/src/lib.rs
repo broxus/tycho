7cd4d295bbbec8363832a7d22f854f560ace745a
Vitaly Terekhov <va.tehb@gmail.com>
2026-03-18 12:41:28 +0000

    feature(wu-tuner): use percentile to clip anchors lag samples; + do not calculate MA until the required range interval is collected; + calculate target wu params by nearest avg anchors lag and wu metrics on the same seqno;

--- wu-tuner/src/lib.rs
+++ wu-tuner/src/lib.rs
@@ -2,6 +2,7 @@ pub use config::WuTunerConfig;
 use tycho_collator::collator::work_units::{MempoolAnchorLag, WuEvent, WuEventData, WuMetrics};
 
 mod config;
+mod rolling_percentile;
 pub mod service;
 mod tuner;
 pub mod updater;
