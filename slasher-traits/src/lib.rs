caed98eec00806dcf0a91b52f5d4e92de3ff9350
Kirill Mikheev <mikheyev.k@gmail.com>
2026-04-15 19:59:53 +0300

    feat(consensus): slasher stats output

--- slasher-traits/src/lib.rs
+++ slasher-traits/src/lib.rs
@@ -1,6 +1,8 @@
+pub use self::mempool::*;
 pub use self::validator::{
     BlockValidationScope, NoopValidatorEventsRecorder, ReceivedSignature, ValidationSessionId,
     ValidatorEvents, ValidatorEventsListener, ValidatorSessionScope,
 };
 
+mod mempool;
 mod validator;
